e4a3476fe3f43238451e8808d47ae3f51e4b5819
Bryan Tripp <bptripp@gmail.com>
2007-08-21 22:30:51 +0000

    making DynamicalSystemSpikeGenerator Probeable

--- simulator/src/java/main/ca/neo/model/neuron/impl/DynamicalSystemSpikeGenerator.java
+++ simulator/src/java/main/ca/neo/model/neuron/impl/DynamicalSystemSpikeGenerator.java
@@ -3,15 +3,21 @@
  */
 package ca.neo.model.neuron.impl;
 
+import java.util.Properties;
+
 import ca.neo.dynamics.DynamicalSystem;
 import ca.neo.dynamics.Integrator;
 import ca.neo.model.InstantaneousOutput;
+import ca.neo.model.Probeable;
+import ca.neo.model.SimulationException;
 import ca.neo.model.SimulationMode;
 import ca.neo.model.Units;
 import ca.neo.model.impl.SpikeOutputImpl;
 import ca.neo.model.neuron.SpikeGenerator;
+import ca.neo.util.MU;
 import ca.neo.util.TimeSeries;
 import ca.neo.util.impl.TimeSeries1DImpl;
+import ca.neo.util.impl.TimeSeriesImpl;
 
 /**
  * A SpikeGenerator for which spiking dynamics are expressed in terms of a DynamicalSystem. 
@@ -20,12 +26,15 @@ import ca.neo.util.impl.TimeSeries1DImpl;
  * 
  * @author Bryan Tripp
  */
-public class DynamicalSystemSpikeGenerator implements SpikeGenerator {
+public class DynamicalSystemSpikeGenerator implements SpikeGenerator, Probeable {
 
 	private static final long serialVersionUID = 1L;
 	
+	public static final String DYNAMICS = "dynamics";
+	
 	private DynamicalSystem myDynamics; 
 	private Integrator myIntegrator;
+	private TimeSeries myDynamicsOutput;
 	private int myVDim;
 	private float mySpikeThreshold;
 	private float myMinIntraSpikeTime;
@@ -46,6 +55,13 @@ public class DynamicalSystemSpikeGenerator implements SpikeGenerator {
 		myVDim = voltageDim;
 		mySpikeThreshold = spikeThreshold;
 		myMinIntraSpikeTime = minIntraSpikeTime;
+
+		Units[] units = new Units[dynamics.getOutputDimension()];
+		for (int i = 0; i < units.length; i++) {
+			units[i] = dynamics.getOutputUnits(i);
+		}
+		myDynamicsOutput = new TimeSeriesImpl(new float[]{0}, MU.uniform(1, dynamics.getOutputDimension(), 0), units);
+	
 	}
 	
 	/**
@@ -55,19 +71,19 @@ public class DynamicalSystemSpikeGenerator implements SpikeGenerator {
 	 */
 	public InstantaneousOutput run(float[] time, float[] current) {
 		boolean spike = false;
-		
-		TimeSeries result = myIntegrator.integrate(myDynamics, new TimeSeries1DImpl(time, current, Units.uAcm2));
-		float[][] values = result.getValues();
+
+		myDynamicsOutput = myIntegrator.integrate(myDynamics, new TimeSeries1DImpl(time, current, Units.uAcm2));
+		float[][] values = myDynamicsOutput.getValues();
 		
 		for (int i = 0; i < values.length && !spike; i++) {
 			if (values[i][myVDim] >= mySpikeThreshold) {
 				boolean crossingThreshold = i > 0 && values[i-1][myVDim] < mySpikeThreshold;
 				boolean nonDuplicateAtStart 
-					= i == 0 && values[1][myVDim] > values[0][myVDim] && result.getTimes()[0] > myLastSpikeTime + myMinIntraSpikeTime;
+					= i == 0 && values[1][myVDim] > values[0][myVDim] && myDynamicsOutput.getTimes()[0] > myLastSpikeTime + myMinIntraSpikeTime;
 				
 				if (crossingThreshold || nonDuplicateAtStart) {
 					spike = true;
-					myLastSpikeTime = result.getTimes()[i];
+					myLastSpikeTime = myDynamicsOutput.getTimes()[i];
 				}
 			}
 		}
@@ -93,6 +109,26 @@ public class DynamicalSystemSpikeGenerator implements SpikeGenerator {
 	 * @see ca.neo.model.SimulationMode.ModeConfigurable#setMode(ca.neo.model.SimulationMode)
 	 */
 	public void setMode(SimulationMode mode) {
+	}
+
+	/** 
+	 * @see ca.neo.model.Probeable#getHistory(java.lang.String)
+	 */
+	public TimeSeries getHistory(String stateName) throws SimulationException {
+		if (stateName.equals(DYNAMICS)) {
+			return myDynamicsOutput;
+		} else {
+			throw new SimulationException("Unknown state: " + stateName);
+		}
+	}
+
+	/** 
+	 * @see ca.neo.model.Probeable#listStates()
+	 */
+	public Properties listStates() {
+		Properties result = new Properties();
+		result.setProperty(DYNAMICS, "Result of spike generation dynamics");
+		return result;
 	}
 	
 }
