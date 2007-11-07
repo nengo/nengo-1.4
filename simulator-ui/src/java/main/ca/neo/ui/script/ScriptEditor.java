b4ce92acca8c826ceb318b6b47aa10c18d51667b
Bryan Tripp <bptripp@gmail.com>
2007-11-07 19:20:03 +0000

    working on script editor

--- simulator-ui/src/java/main/ca/neo/ui/script/ScriptEditor.java
+++ simulator-ui/src/java/main/ca/neo/ui/script/ScriptEditor.java
@@ -4,15 +4,22 @@
 package ca.neo.ui.script;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.List;
 
+import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.text.JTextComponent;
 
 /**
  * A basic text editor. 
@@ -22,6 +29,7 @@ import javax.swing.JTabbedPane;
 public class ScriptEditor extends JPanel {
 	
 	private JTabbedPane myTabs;
+	private List<ScriptData> myScripts; //TODO
 
 	public ScriptEditor() {
 		setLayout(new BorderLayout());
@@ -30,9 +38,82 @@ public class ScriptEditor extends JPanel {
 		add(myTabs, BorderLayout.CENTER);		
 	}
 	
+	public void newFile() {
+		openEditor("New Script");
+	}
+	
+	public void saveCurrentFile() {
+		System.out.println("saving ... ");
+		
+		//TODO
+	}
+	
+	public void saveCurrentFileAs() {
+		System.out.println("saving as ... ");
+		JFileChooser fileChooser = new JFileChooser(); //TODO: set working directory
+		int selection = fileChooser.showSaveDialog(this);
+		if (selection == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+		}
+	}
+	
+	public void closeCurrentFile() {
+		System.out.println("closing ... ");
+		myTabs.remove(myTabs.getSelectedComponent());
+	}
+	
+	public void openFile() {
+		System.out.println("opening ... ");
+		//TODO
+	}
+	
+	private void openEditor(String name) {
+		JEditorPane ep = new JEditorPane();
+		myTabs.addTab(name, ep);
+	}
+	
+	/**
+	 * Data related to a single open script file. 
+	 * 
+	 * @author Bryan Tripp
+	 */
+	private class ScriptData {
+		
+		private JTextComponent myTextComponent;
+		private File myFile;
+		private boolean mySaved;
+		
+		public ScriptData(JTextComponent textComponent, File file, boolean saved) {
+			myTextComponent = textComponent;
+			myFile = file;
+			mySaved = saved;
+		}
+		
+		public JTextComponent getTextComponent() {
+			return myTextComponent;
+		}
+		
+		public File getFile() {
+			return myFile;
+		}
+		
+		public void setFile(File file) {
+			myFile = file;
+		}
+		
+		public boolean isSaved() {
+			return mySaved;
+		}
+		
+		public void setSaved(boolean saved) {
+			mySaved = saved;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		ScriptEditor editor = new ScriptEditor();
+		final ScriptEditor editor = new ScriptEditor();
 		
 		JFrame frame = new JFrame("Script Editor");
 		frame.getContentPane().add(editor);
@@ -40,11 +121,34 @@ public class ScriptEditor extends JPanel {
 		JMenuBar menuBar = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
 		JMenuItem newItem = new JMenuItem("New");
+		newItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editor.newFile();
+			}
+		});
 		fileMenu.add(newItem);
 		JMenuItem openItem = new JMenuItem("Open");
+		openItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editor.openFile();
+			}
+		});
 		fileMenu.add(openItem);
-		JMenuItem saveItem = new JMenuItem("Save");
+		JMenuItem saveItem = new JMenuItem("Save As ...");
+		saveItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editor.saveCurrentFileAs();
+			}
+		});
 		fileMenu.add(saveItem);		
+		menuBar.add(fileMenu);
+		JMenuItem closeItem = new JMenuItem("Close");
+		closeItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editor.closeCurrentFile();
+			}
+		});
+		fileMenu.add(closeItem);		
 		menuBar.add(fileMenu);
 		
 		frame.setJMenuBar(menuBar);
