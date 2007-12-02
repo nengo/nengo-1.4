c04af345b42023e5563604fcacd899a3497615aa
Bryan Tripp <bptripp@gmail.com>
2007-12-02 00:25:07 +0000

    Plotter test

--- simulator/src/java/test/ca/neo/plot/PlotterFunctional.java
+++ simulator/src/java/test/ca/neo/plot/PlotterFunctional.java
@@ -3,9 +3,15 @@
  */
 package ca.neo.plot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ca.neo.model.Units;
+import ca.neo.util.MU;
+import ca.neo.util.SpikePattern;
 import ca.neo.util.TimeSeries;
 import ca.neo.util.TimeSeries1D;
+import ca.neo.util.impl.SpikePatternImpl;
 import ca.neo.util.impl.TimeSeries1DImpl;
 import ca.neo.util.impl.TimeSeriesImpl;
 
@@ -15,7 +21,7 @@ import ca.neo.util.impl.TimeSeriesImpl;
  */
 public class PlotterFunctional {
 
-	public static void main(String[] args) {
+	public static void basicTest() {
 		float[] times = new float[]{1f, 2f, 3f, 4f, 5f};
 		float[] v1 = new float[]{1f, 2f, 3f, 2f, 1f};
 		float[][] v3 = new float[][]{
@@ -31,6 +37,35 @@ public class PlotterFunctional {
 		
 		TimeSeries s3 = new TimeSeriesImpl(times, v3, new Units[]{Units.UNK, Units.UNK, Units.UNK}); 
 		Plotter.plot(s3, "test2");
+	}
+	
+	public static void multiTest() {
+		List<TimeSeries> series = new ArrayList<TimeSeries>(10);
+		float[] times = MU.makeVector(1, 1, 10);
+		float[][] values1 = new float[][]{MU.makeVector(.1f, .1f, 1), MU.makeVector(.2f, .1f, 1.1f)};
+		float[][] values2 = new float[][]{MU.makeVector(1.1f, .1f, 2), MU.makeVector(1.2f, .1f, 2.1f)};
+		float[][] values3 = new float[][]{MU.makeVector(2.1f, .1f, 3), MU.makeVector(2.2f, .1f, 3.1f)};
+		Units[] units = Units.uniform(Units.UNK, 2);
+		series.add(new TimeSeriesImpl(times, MU.transpose(values1), units));
+		series.add(new TimeSeriesImpl(times, MU.transpose(values2), units));
+		series.add(new TimeSeriesImpl(times, MU.transpose(values3), units));
+		
+		List<SpikePattern> patterns = new ArrayList<SpikePattern>(10);
+		SpikePatternImpl p1 = new SpikePatternImpl(2);
+		p1.addSpike(0, 1);
+		p1.addSpike(1, 2);
+		patterns.add(p1);
+		SpikePatternImpl p2 = new SpikePatternImpl(3);
+		p2.addSpike(0, 5);
+		p2.addSpike(1, 6);
+		p2.addSpike(2, 7);
+		patterns.add(p2);
+		
+		Plotter.plot(series, patterns, "multi series");
+	}
+	
+	public static void main(String[] args) {
+		multiTest();
 	}
 
 }
