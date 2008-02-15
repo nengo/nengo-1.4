fe79e81a132fda9a2bbbe7cce1439191648d5242
Bryan Tripp <bptripp@gmail.com>
2008-02-15 23:22:36 +0000

    supporting node name changes

--- simulator-ui/src/java/main/ca/neo/ui/models/constructors/CNetwork.java
+++ simulator-ui/src/java/main/ca/neo/ui/models/constructors/CNetwork.java
@@ -1,7 +1,9 @@
 package ca.neo.ui.models.constructors;
 
 import ca.neo.model.Node;
+import ca.neo.model.StructuralException;
 import ca.neo.model.impl.NetworkImpl;
+import ca.neo.ui.configurable.ConfigException;
 import ca.neo.ui.configurable.PropertyDescriptor;
 import ca.neo.ui.configurable.PropertySet;
 import ca.neo.ui.configurable.descriptors.PString;
@@ -16,10 +18,14 @@ public class CNetwork extends Constructable {
 	private static final PropertyDescriptor[] zConfig = { pName };
 
 	@Override
-	protected Node configureModel(PropertySet configuredProperties) {
+	protected Node configureModel(PropertySet configuredProperties) throws ConfigException {
 
 		NetworkImpl network = new NetworkImpl();
-		network.setName((String) configuredProperties.getProperty(pName));
+		try {
+			network.setName((String) configuredProperties.getProperty(pName));
+		} catch (StructuralException e) {
+			throw new ConfigException(e.getMessage());
+		}
 
 		return network;
 	}
