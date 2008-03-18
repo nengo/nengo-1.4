8899965cbeca99f7628c0b55282561b3aa23daa3
Bryan Tripp <bptripp@gmail.com>
2008-03-18 21:35:02 +0000

    removing references to methods in Plotter that aren't there any more (not needed for this case anyway)

--- simulator-ui/src/java/main/ca/neo/ui/util/DialogPlotter.java
+++ simulator-ui/src/java/main/ca/neo/ui/util/DialogPlotter.java
@@ -4,8 +4,6 @@
 package ca.neo.ui.util;
 
 import java.awt.BorderLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -13,7 +11,6 @@ import javax.swing.JPanel;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 
-import ca.neo.plot.Plotter;
 import ca.neo.plot.impl.DefaultPlotter;
 
 /**
@@ -32,20 +29,11 @@ public class DialogPlotter extends DefaultPlotter {
 	}
 
 	@Override
-	protected void showChart(JFreeChart chart, String title) {
-		
+	protected void showChart(JFreeChart chart, String title) {		
 		JPanel panel = new ChartPanel(chart);
 
-		openingPlot();
 		JDialog dialog = new JDialog(parent, title);
 		dialog.getContentPane().add(panel, BorderLayout.CENTER);
-
-		final Plotter plotter = this;
-		dialog.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				plotter.closingPlot();
-			}
-		});
 
 		dialog.pack();
 		dialog.setVisible(true);
