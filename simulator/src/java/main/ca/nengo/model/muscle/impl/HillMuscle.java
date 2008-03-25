ddd4d5193a5ab1fbec94a5b8b430c95981782ff3
Bryan Tripp <bptripp@gmail.com>
2008-03-25 23:22:05 +0000

    adding license header to each source file

--- simulator/src/java/main/ca/nengo/model/muscle/impl/HillMuscle.java
+++ simulator/src/java/main/ca/nengo/model/muscle/impl/HillMuscle.java
@@ -1,4 +1,30 @@
 /*
+The contents of this file are subject to the Mozilla Public License Version 1.1 
+(the "License"); you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis, WITHOUT
+WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+language governing rights and limitations under the License.
+
+The Original Code is "HillMuscle.java". Description: 
+"A Hill-type muscle model.
+ 
+  Use RootFinder with function f_CE - f_SE, parameter l_SE, range 0 to breaking length"
+
+The Initial Developer of the Original Code is Bryan Tripp & Centre for Theoretical Neuroscience, University of Waterloo. Copyright (C) 2006-2008. All Rights Reserved.
+
+Alternatively, the contents of this file may be used under the terms of the GNU 
+Public License license (the GPL License), in which case the provisions of GPL 
+License are applicable  instead of those above. If you wish to allow use of your 
+version of this file only under the terms of the GPL License and not to allow 
+others to use your version of this file under the MPL, indicate your decision 
+by deleting the provisions above and replace  them with the notice and other 
+provisions required by the GPL License.  If you do not delete the provisions above,
+a recipient may use your version of this file under either the MPL or the GPL License.
+*/
+
+/*
  * Created on 25-Nov-2006
  */
 package ca.nengo.model.muscle.impl;
