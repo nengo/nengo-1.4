05c3fff0a44f1ad6b7df70ccc0456e2d07300d28
Daniel Rasmussen <drasmuss@uwaterloo.ca>
2008-05-04 01:42:10 +0000

    rough work

--- simulator/src/java/test/ca/nengo/plot/ScopeExample.java
+++ simulator/src/java/test/ca/nengo/plot/ScopeExample.java
@@ -10,6 +10,7 @@ import java.awt.geom.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
+import java.lang.reflect.*;
 import java.util.*;
 
 import javax.swing.SwingUtilities;
@@ -20,8 +21,8 @@ import javax.imageio.ImageIO;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.plot.*;
+import org.jfree.chart.renderer.xy.*;
 import org.jfree.data.Range;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -55,17 +56,26 @@ public class ScopeExample {
 			Network network = new NetworkImpl();
 			
 			//this "function input" is Probeable ... 		
-			Function f1 = new PostfixFunction("sin(x0)", 1);
-			Function f2 = new PostfixFunction("sin(x0)^2", 1);
+			Function[] funcs = new Function[2];
+			for(int i=0; i<funcs.length; ++i) {
+				funcs[i] = new PostfixFunction("sin(x0)^"+(i+1), 1);
+			}
 			String name = "functions of time";
-			FunctionInput fi = new FunctionInput(name, new Function[]{f1, f2}, Units.uAcm2);
+			FunctionInput fi = new FunctionInput(name, funcs, Units.uAcm2);
 			network.addNode(fi);
 			
 			//we can add a probe to it and run the simulator ... 
 			Probe p = network.getSimulator().addProbe(name, FunctionInput.STATE_NAME, true);			
 			network.run(0, 10);
 			
-			animPlotVector(p.getData(), "vector plot", true, 30);
+			JFrame frame = makeAnimPlotFrame("vector plot");
+			frame.setLayout(new BorderLayout());
+			Scope scope = new Scope(p, 100, 20);
+			frame.getContentPane().add(scope.getGraphPanel(), BorderLayout.CENTER);
+			frame.getContentPane().add(scope.getCtrlPanel(), BorderLayout.SOUTH);
+			//frame.setSize(900, 600);
+			frame.pack();
+			frame.setLocationRelativeTo(null);
 
 			//... and plot the probed data from the simulation ... 
 			//Plotter.plot(p.getData(), "function output");
@@ -102,91 +112,5 @@ public class ScopeExample {
         r.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         r.setVisible(true);
         return r;
-	}
-	
-	private static List<ChartPanel> makeAnimPlotVectorChartPanels(TimeSeries timeSeries, 
-			boolean scaleRangAxisConstant, int trailLength) {
-        List<ChartPanel> r = new Vector<ChartPanel>();
-        float[][] values = timeSeries.getValues();
-        final int timeInc = 100;
-        for(int curTime=0; curTime<values.length; curTime+=timeInc) {
-	        XYSeriesCollection dataset = new XYSeriesCollection();
-	        for(int trailPosTime=curTime; 
-	        		trailPosTime>=curTime-trailLength*timeInc && trailPosTime>=0; 
-	        		trailPosTime-=timeInc) {
-				XYSeries series = new XYSeries("trail "+trailPosTime);
-				for(int vecDim=0; vecDim<values[trailPosTime].length; ++vecDim) {
-					series.add(trailPosTime, values[trailPosTime][vecDim]);
-				}
-				dataset.addSeries(series);
-	        }
-			JFreeChart chart = ChartFactory.createScatterPlot("Function", "Input", "Output",
-					dataset, PlotOrientation.VERTICAL, false, false, false);
-			animPlotVectorSetupRenderer(chart.getXYPlot().getRenderer(), trailLength);
-			r.add(new ChartPanel(chart));
-        }
-        if(scaleRangAxisConstant) {
-        	scaleChartPanelsRangeAxisToConstant(r);
-        }
-        return r;
-	}
-	
-	private static void animPlotVectorSetupRenderer(XYItemRenderer renderer, int trailLength) {
-		for(int i=0; i<=trailLength; ++i) {
-			int c = 255*i/trailLength;
-			renderer.setSeriesPaint(i, new Color(c, c, c));
-			renderer.setSeriesShape(i, new Ellipse2D.Float(-3, -3, 6, 6));
-		}
-	}
-	
-	private static void scaleChartPanelsRangeAxisToConstant(List<ChartPanel> chartPanels) {
-		double min=Double.MAX_VALUE, max=Double.MIN_VALUE;
-        for(Iterator<ChartPanel> it=chartPanels.iterator(); it.hasNext(); ) {
-        	final ChartPanel curPanel = it.next();
-        	Range curRange = curPanel.getChart().getXYPlot().getRangeAxis().getRange();
-        	if(min > curRange.getLowerBound()) {
-        		min = curRange.getLowerBound();
-        	}
-        	if(max < curRange.getUpperBound()) {
-        		max = curRange.getUpperBound();
-        	}
-        }
-        for(Iterator<ChartPanel> it=chartPanels.iterator(); it.hasNext(); ) {
-        	final ChartPanel curPanel = it.next();
-        	curPanel.getChart().getXYPlot().getRangeAxis().setRange(min, max);
-        }        
-	}
-	
-	private static void animPlotShowChartPanels(final JFrame frame, List<ChartPanel> chartPanels) {
-        int i=0;
-        for(Iterator<ChartPanel> it=chartPanels.iterator(); it.hasNext(); ) {
-        	final ChartPanel panel = it.next();
-        	final boolean firstTimeAround = i==0; 
-        	SwingUtilities.invokeLater(new Runnable() { public void run() {
-				frame.getContentPane().removeAll();
-				frame.getContentPane().add(panel, BorderLayout.CENTER);
-				if(firstTimeAround) {
-					frame.pack();
-				} else {
-					frame.validate();
-				}
-				//System.out.println(panel.getChart().getXYPlot().getRenderer().getSeriesPaint(2)); 
-        	}});
-			try {
-				Thread.sleep(100);
-			} catch(InterruptedException e) {}
-        }
-	}
-	
-	private static void animPlotVector(TimeSeries timeSeries, String title, 
-			boolean scaleRangAxisConstant, int trailLength) throws IOException {
-		assert trailLength >= 0;
-		List<ChartPanel> chartPanels = makeAnimPlotVectorChartPanels(timeSeries, 
-				scaleRangAxisConstant, trailLength);
-		animPlotShowChartPanels(makeAnimPlotFrame(title), chartPanels);
-	}
-	
-	private static Vector v(float[] a_) {
-		return new Vector(Arrays.asList(a_));
 	}
 }
