2be657ffaa776bdc1e772a07c37021fbe62dced6
Xuan Choo <xchoo.mainframe@gmail.com>
2009-12-06 00:59:12 +0000

    Removed un-needed file input.py - Bug fix in components/__init__.py

--- simulator-ui/python/timeview/components/__init__.py
+++ simulator-ui/python/timeview/components/__init__.py
@@ -2,7 +2,6 @@ import grid
 import item
 import graph
 import vector_grid
-import input
 import item
 import spike_raster
 import function
@@ -13,8 +12,7 @@ import xyplot
 from grid import Grid
 from item import Item
 from graph import Graph
-from vector_grid import VectorGridfrom input import Input
-from spike_raster import SpikeRasterfrom function import FunctionControl
+from vector_grid import VectorGridfrom spike_raster import SpikeRasterfrom function import FunctionControl
 from view3d import View3D
 from preferred import PreferredDirection
 from xyplot import XYPlot
