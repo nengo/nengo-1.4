31f52367b9c04d04225e36eaf0e4dd7eeca433cb
Terry Stewart <terry.stewart@gmail.com>
2010-05-05 03:05:35 +0000

    - fixed fft import, used existing identity function

--- simulator-ui/python/numeric.py
+++ simulator-ui/python/numeric.py
@@ -1,12 +1,13 @@
 from ca.nengo.ui.JNumeric.JNumeric import *
 
+
+# gets fft() and inverse_fft()
+from ca.nengo.ui.JNumeric.JNumeric.FFT import fft,inverse_fft
+
 def diag(values):
     a=zeros((len(values),len(values)))
     for i,v in enumerate(values):
         a[i][i]=v
     return a
 
-def eye(i):
-    return diag([1.0]*i)
-    
-    
+eye=identity    
