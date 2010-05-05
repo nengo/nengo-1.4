7082a50aabff861d72e0991c2dc4a4fb587fa08a
Terry Stewart <terry.stewart@gmail.com>
2010-05-05 04:22:32 +0000

    - changed name of inverse fft function

--- simulator-ui/python/numeric.py
+++ simulator-ui/python/numeric.py
@@ -2,10 +2,10 @@ from ca.nengo.ui.JNumeric.JNumeric import *
 
 
 # gets fft() and inverse_fft()
-from ca.nengo.ui.JNumeric.JNumeric.FFT import fft,inverse_fft
+from ca.nengo.ui.JNumeric.JNumeric.FFT import fft,ifft
 
-def diag(values):
-    a=zeros((len(values),len(values)))
+def diag(values,typecode='f'):
+    a=zeros((len(values),len(values)),typecode=typecode)
     for i,v in enumerate(values):
         a[i][i]=v
     return a
