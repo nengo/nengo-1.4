3b1b208f334c5185507ac938dec83a44da934018
Terry Stewart <terry.stewart@gmail.com>
2011-03-10 20:21:05 +0000

    - reorganized toolbar - added simulation mode control

--- simulator-ui/python/toolbar.py
+++ simulator-ui/python/toolbar.py
@@ -8,26 +8,72 @@ if 'lib/iText-5.0.5.jar' not in sys.path:
     sys.path.append('lib/iText-5.0.5.jar')
 
 
+from ca.nengo.model import SimulationMode
+class SimulationModeComboBox(JComboBox):
+    def __init__(self):
+        JComboBox.__init__(self,['spiking','rate','direct'],toolTipText='set simulation mode')
+        self.node=None
+        self.enabled=False
+        self.addActionListener(self)
+        self.maximumSize=self.preferredSize
+    def set_node(self,node):
+        self.node=None
+        if node is not None and not hasattr(node.model,'mode'): node=None
+        self.enabled=node is not None
+        
+        if node is not None:
+            mode=node.model.mode
+            
+            if mode in [SimulationMode.DEFAULT,SimulationMode.PRECISE]:
+                self.setSelectedIndex(0)
+            elif mode in [SimulationMode.RATE]:
+                self.setSelectedIndex(1)
+            elif mode in [SimulationMode.DIRECT,SimulationMode.APPROXIMATE]:
+                self.setSelectedIndex(2)
+
+            self.node=node
+
+        
+    def actionPerformed(self,event):
+        if self.node is None: return
+        mode=self.getSelectedItem()
+        if mode=='rate': self.node.model.mode=SimulationMode.RATE
+        elif mode=='direct': self.node.model.mode=SimulationMode.DIRECT
+        else: self.node.model.mode=SimulationMode.DEFAULT
+        self.set_node(self.node)
+    
 
 
-class ToolBar(ca.nengo.ui.lib.world.handlers.SelectionHandler.SelectionListener,ca.nengo.ui.lib.world.WorldObject.ChildListener):
+class ToolBar(ca.nengo.ui.lib.world.handlers.SelectionHandler.SelectionListener,ca.nengo.ui.lib.world.WorldObject.ChildListener,java.lang.Runnable):
     def __init__(self):
         self.ng=ca.nengo.ui.NengoGraphics.getInstance()
         self.toolbar=JToolBar("Nengo actions",floatable=False)
-        
-        self.button_run=self.make_button('play',self.do_run,'run',enabled=False)
-        self.button_pdf=self.make_button('pdf',self.do_pdf,'save as pdf')
         self.toolbar.add(self.make_button('open',self.do_open,'open file'))
-        self.toolbar.add(self.button_run)
-        self.toolbar.add(self.button_pdf)
+        self.toolbar.add(self.make_button('pdf',self.do_pdf,'save as pdf'))
+
+        self.toolbar.add(Box.createHorizontalGlue())
+        
+        self.mode_combobox=SimulationModeComboBox()
+        self.toolbar.add(self.mode_combobox)
+
+        self.toolbar.add(Box.createHorizontalGlue())
         self.toolbar.add(self.make_button('inspect',self.do_inspect,'inspect'))
         self.toolbar.add(self.make_button('console',self.do_console,'toggle console'))
+        self.button_run=self.make_button('interactive',self.do_run,'interactive plots',enabled=False)
+        self.toolbar.add(self.button_run)
 
         ca.nengo.ui.lib.world.handlers.SelectionHandler.addSelectionListener(self)
         self.ng.getWorld().getGround().addChildrenListener(self)
 
         self.ng.getContentPane().add(self.toolbar,BorderLayout.PAGE_START)
 
+        java.lang.Thread(self).start()
+
+    
+    def run(self):
+        while True:
+            self.update()
+            java.lang.Thread.sleep(1000)
         
         
 
@@ -47,6 +93,7 @@ class ToolBar(ca.nengo.ui.lib.world.handlers.SelectionHandler.SelectionListener,
         
     def update(self):
         net=self.get_current_network()
+        self.mode_combobox.set_node(self.ng.getSelectedObj())
         if net is None:
             self.button_run.enabled=False
             self.button_run.toolTipText='run'
@@ -74,6 +121,10 @@ class ToolBar(ca.nengo.ui.lib.world.handlers.SelectionHandler.SelectionListener,
     def do_inspect(self,event):
         pane=self.ng.configPane.toJComponent()
         pane.auxVisible=not pane.auxVisible
+        if pane.auxVisible:
+            model=self.ng.getSelectedObj()
+            if model is not None: model=model.model
+            self.ng.configPane.configureObj(model)
         
     def do_open(self,event):
         ca.nengo.ui.actions.OpenNeoFileAction(ng).doAction()
