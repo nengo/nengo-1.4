7d3b717874652766ece16228226b07bc2279e70a
Eric Crawford <e2crawfo@uwaterloo.ca>
2011-03-24 21:41:43 +0000

    Reflecting replacement of GPUNodeThreadPool with NEF_GPU_Interface.

--- simulator-ui/python/timeview/simulator.py
+++ simulator-ui/python/timeview/simulator.py
@@ -10,33 +10,43 @@ from ca.nengo.util.impl import NodeThreadPool, NEF_GPU_Interface
 class Simulator:
     def __init__(self,network):
         self.nodes=[]
+        self.projections=[]
         self.network=network
         self.initialize(network)
         
         if NEF_GPU_Interface.getUseGPU():
-            for n in self.nodes: 
-                if isinstance(n,NEFEnsemble) and n.mode==SimulationMode.DEFAULT: 
-                    n.setGPU(True)
-            self.thread_pool=NEF_GPU_Interface(self.nodes)
-        elif NodeThreadPool.isMultithreading():
-            self.thread_pool=NodeThreadPool(self.nodes)
+            #for n in self.nodes: 
+        #        if isinstance(n,NEFEnsemble) and n.mode==SimulationMode.DEFAULT: 
+        #            n.setGPU(True)
+
+            self.thread_pool=NEF_GPU_Interface(self.nodes, self.projections)
+        #elif NodeThreadPool.isMultithreading():
+        #    self.thread_pool=NodeThreadPool(self.nodes, self.projections)
         else:    
             self.thread_pool=None
+
     def initialize(self,network):
+        
+        for p in network.projections:
+          self.projections.append(p);
+
         for n in network.nodes:
-            if n.__class__.__name__=='NetworkImpl':
+            if n.__class__.__name__=='NetworkImpl' or n.__class__.__name__=='NetworkArray':
             #if isinstance(n,Network) and not n.__class__.__name__ in ['CCMModelNetwork','PyramidalNetwork']:
                 self.initialize(n)
             else:
                 self.nodes.append(n)
+
     def reset(self,randomize=False):
         for n in self.nodes: n.reset(randomize)
+
     def step(self,start,end):
-        for p in self.network.projections:
-            p.termination.setValues(p.origin.getValues())
         if self.thread_pool is not None:
+            # the thread pool should take care of the projections
             self.thread_pool.step(start,end)
         else:    
+            for p in self.projections:
+              p.termination.setValues(p.origin.getValues())
             for n in self.nodes:
                 n.run(start,end)    
 
