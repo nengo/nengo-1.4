6c2f2b5bb0ca91fd4ac3d0339929d010575852ae
Terry Stewart <terry.stewart@gmail.com>
2011-05-03 21:08:13 +0000

    - added special case for more general use of script

--- simulator-ui/python/nef/templates/basalganglia.py
+++ simulator-ui/python/nef/templates/basalganglia.py
@@ -14,9 +14,10 @@ def test_params(net,p):
 import nps.basalganglia
 import nef
 import numeric
-def make(net,name='Basal Ganglia',dimensions=1,pstc=0.01):
+def make(net,name='Basal Ganglia',dimensions=1,pstc=0.01,netbg=None):
 
-    netbg=nef.Network(name)
+    if netbg is None:
+        netbg=nef.Network(name)
     input=netbg.make('input',1,dimensions,quick=True,mode='direct')
     output=netbg.make('output',1,dimensions,quick=True,mode='direct')
     nps.basalganglia.make_basal_ganglia(netbg,input,output,dimensions)
@@ -25,6 +26,6 @@ def make(net,name='Basal Ganglia',dimensions=1,pstc=0.01):
     netbg.network.exposeTermination(input.getTermination('input'),'input')
     netbg.network.exposeOrigin(output.getOrigin('X'),'output')
     
-
-    net.add(netbg.network)
+    if net is not None:
+        net.add(netbg.network)
     
