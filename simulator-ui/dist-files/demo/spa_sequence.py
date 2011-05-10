cb5871e41346c16651bdbc67c3d3fe69db956179
Terry Stewart <terry.stewart@gmail.com>
2011-05-10 23:11:02 +0000

    - demos for SPA system

---
+++ simulator-ui/dist-files/demo/spa_sequence.py
@@ -0,0 +1,28 @@
+from spa import *
+
+D=16
+
+class Rules:
+    def A(state='A'):
+        set(state='B')
+    def B(state='B'):
+        set(state='C')
+    def C(state='C'):
+        set(state='D')
+    def D(state='D'):
+        set(state='E')
+    def E(state='E'):
+        set(state='A')
+    
+
+
+class Sequence(SPA):
+    dimensions=16
+    
+    state=Buffer()
+    BG=BasalGanglia(Rules())
+    thal=Thalamus(BG)
+    
+    input=Input(0.1,state='D')
+
+seq=Sequence()
