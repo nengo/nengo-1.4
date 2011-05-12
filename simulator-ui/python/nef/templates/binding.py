ff76d8027e813acc74ca1af60a2229e89aa344cc
Matthew Gingerich <mjhginge@engmail.uwaterloo.ca>
2011-05-12 19:55:48 +0000

    Simpler drag-and-drop binding component.

--- simulator-ui/python/nef/templates/binding.py
+++ simulator-ui/python/nef/templates/binding.py
@@ -4,43 +4,28 @@ icon='convolution.png'
 
 params=[
     ('name','Name',str),
-    ('nameA','Name of input ensemble A',str),
-    ('nameB','Name of input ensemble B',str),
-    ('nameC','Name of output ensemble',str),
-    ('dimensions','Number of dimensions',int),
+    ('outputName','Name of output ensemble',str),
     ('N_per_D','Number of neurons per dimension',int),
     ('invertA','Invert input A',bool),
     ('invertB','Invert input B',bool),
-    ('pstc','Time constant for input',float),
     ]
 
 def test_params(net,p):
-    aIsSet = False
-    bIsSet = False
-    cIsSet = False
+    outputIsSet = False
     nameIsTaken = False
     nodeList = net.network.getNodes()
     for i in nodeList:
-        if i.name == p['nameA']:
-            aIsSet = True
-        elif i.name == p['nameB']:
-            bIsSet = True
-        elif i.name == p['nameC']:
-            cIsSet = True
+        if i.name == p['outputName']:
+            outputIsSet = True
         elif i.name == p['name']:
             nameIsTaken = True
     if nameIsTaken: return 'That name is already taken'
-    if not (aIsSet and bIsSet and cIsSet): return 'Must provide names of existing input/output ensembles'
-    if p['dimensions']<1: return 'Must have a positive number of dimensions'
+    if not outputIsSet: return 'Must provide the name of an existing output ensemble'
     
 import nef
 import nef.convolution
 import numeric
-def make(net,name='Bind',nameA='A', nameB='B', nameC='C', dimensions=20, N_per_D=300, invertA=False, invertB=False, pstc=0.01):
+def make(net,name='Bind', outputName='C', N_per_D=300, invertA=False, invertB=False):
 
-    netconv = nef.Network(name)
-    inputA=net.network.getNode(nameA)
-    inputB=net.network.getNode(nameB)
-    output=net.network.getNode(nameC)
-    
-    nef.convolution.make_convolution(netconv, '*', inputA, inputB, output, N_per_D, quick=True, invert_first=invertA, invert_second=invertB)
+    output=net.network.getNode(outputName) 
+    nef.convolution.make_convolution(net, name, None, None, output, int(N_per_D/2+1), quick=True, invert_first=invertA, invert_second=invertB)
