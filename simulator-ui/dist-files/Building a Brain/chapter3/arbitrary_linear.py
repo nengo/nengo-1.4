92c58222ed0a7318c95069d90d2dc47070d3618f
Matthew Gingerich <mjhginge@engmail.uwaterloo.ca>
2011-06-20 23:35:16 +0000

    Demo script edits (arbitrary_linear.py) and layout.

--- simulator-ui/dist-files/Building a Brain/chapter3/arbitrary_linear.py
+++ simulator-ui/dist-files/Building a Brain/chapter3/arbitrary_linear.py
@@ -1,9 +1,9 @@
 import nef
 
 net=nef.Network('Arbitrary Linear Transformations')
-input=net.make_input('input',[0, 0, 0, 0, 0])
-A=net.make('A',200,5,quick=True)
-B=net.make('B',200,4,quick=True)
+input=net.make_input('input',[0.5, -0.5])
+A=net.make('A',200,2,quick=True)
+B=net.make('B',200,3,quick=True)
 net.connect(input,A)
-net.connect(A, B, transform=[[1, 0, 0, 0, 0.1], [0, 1, 0, 0, 0.2], [0, 0, 1, 0, 0.3], [0, 0, 0, 1, 0.4]])
+net.connect(A, B, transform=[[0, 1], [1, 0], [0.5, 0.5]])
 net.add_to(world)
