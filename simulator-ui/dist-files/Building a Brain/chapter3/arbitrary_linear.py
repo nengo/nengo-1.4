e8dc40ada226db836faea2140b0f56bce08fafa9
Matthew Gingerich <mjhginge@engmail.uwaterloo.ca>
2011-06-21 14:38:50 +0000

    Name change in demo script to match book.

--- simulator-ui/dist-files/Building a Brain/chapter3/arbitrary_linear.py
+++ simulator-ui/dist-files/Building a Brain/chapter3/arbitrary_linear.py
@@ -2,8 +2,8 @@ import nef
 
 net=nef.Network('Arbitrary Linear Transformations')
 input=net.make_input('input',[0.5, -0.5])
-A=net.make('A',200,2,quick=True)
-B=net.make('B',200,3,quick=True)
-net.connect(input,A)
-net.connect(A, B, transform=[[0, 1], [1, 0], [0.5, 0.5]])
+x=net.make('x',200,2,quick=True)
+z=net.make('z',200,3,quick=True)
+net.connect(input,x)
+net.connect(x, z, transform=[[0, 1], [1, 0], [0.5, 0.5]])
 net.add_to(world)
