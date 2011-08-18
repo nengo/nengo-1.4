3e89d08e49a9b35f02efb98dfd4da336af836710
Chris Eliasmith <celiasmith@uwaterloo.ca>
2011-08-18 21:46:38 +0000

    oscillator demo

---
+++ simulator-ui/dist-files/demo/oscillator.py
@@ -0,0 +1,9 @@
+import nef
+
+net=nef.Network('Oscillator')
+input=net.make_input('input',[0,0])
+input.functions=[ca.nengo.math.impl.PiecewiseConstantFunction([0.2,0.3],[0,1,0]),ca.nengo.math.impl.PiecewiseConstantFunction([0],[0,0])]
+A=net.make('A',200,2,quick=False)
+net.connect(input,A,weight=1,pstc=0.1)
+net.connect(A,A,[[1,1],[-1,1]],pstc=0.1)
+net.add_to(world)
