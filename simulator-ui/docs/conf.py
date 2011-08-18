ac67670ce582f9533c67dbaeb86687ae22d06863
Terry Stewart <terry.stewart@gmail.com>
2011-08-18 19:15:07 +0000

    - fixed configuration file

--- simulator-ui/docs/conf.py
+++ simulator-ui/docs/conf.py
@@ -17,7 +17,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('../workspace2/simulator-ui/python')
+sys.path.append('../python')
 sys.path.append('dummy')
 
 # -- General configuration -----------------------------------------------------
