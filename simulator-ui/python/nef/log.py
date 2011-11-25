e4e7d40101c56ace3ae72bf9f6745453b8bb56fe
Terry Stewart <terry.stewart@gmail.com>
2011-11-25 23:46:06 +0000

    - new cleaner approach to log (with changed file format)

--- simulator-ui/python/nef/log.py
+++ simulator-ui/python/nef/log.py
@@ -2,140 +2,169 @@ import time
 import numeric
 from simplenode import SimpleNode
 from nef_core import Network
- 
+from array import NetworkArray
+import hrr
+from ca.nengo.model import Origin
 
-class LogVector:
-    def __init__(self,origin,tau):
+class LogBasic:
+    def __init__(self,name,origin):
+        self.name=name
         self.origin=origin
-        self.tau=tau
         self.init()
     def init(self):
         self.value=numeric.array([0]*self.length(),typecode='f')
     def length(self):
         return len(self.data())
     def type(self):
-        return 'v%d'%self.length()
+        return 'v<%d>'%self.length()
     def data(self):
-        return self.origin.getValues().getValues()
+        if isinstance(self.origin,list):
+            r=[]
+            for o in self.origin: r.extend(o.getValues().getValues())
+            return r
+        else:    
+            return self.origin.getValues().getValues()
+    def tick(self,dt):
+        self.value=self.data()
+    def text(self):
+        return ';'.join(['%1.3f'%x for x in self.value])
+    def flush(self):
+        pass    
+ 
+
+class LogVector(LogBasic):
+    def __init__(self,name,origin,tau):
+        LogBasic.__init__(self,name,origin)
+        self.tau=tau
+    def init(self):
+        self.value=numeric.array([0]*self.length(),typecode='f')
     def tick(self,dt):
         self.value*=(1.0-dt/self.tau)
         self.value+=numeric.array(self.data())*(dt/self.tau)    
-    def text(self):
-        return '[%s]'%(';'.join(['%1.3f'%x for x in self.value]))
 
 class LogVocab(LogVector):
-    def __init__(self,origin,tau,vocab,terms=None):
-        LogVector.__init__(self,origin,tau)
+    def __init__(self,name,origin,tau,vocab,terms=None):
+        LogVector.__init__(self,name,origin,tau)
         self.vocab=vocab
         self.terms=terms
     def text(self):
         return self.vocab.text(self.value,terms=self.terms)
+    def type(self):
+        return 'vocab'
         
-class LogSpikeCount:
-    def __init__(self,origin):
-        self.origin=origin
-        self.init()
+class LogSpikeCount(LogBasic):
+    def __init__(self,name,origin,skip=0):
+        self.skip=skip
+        LogBasic.__init__(self,name,origin)
     def init(self):
         self.value=numeric.array([0]*self.length(),typecode='d')
     def length(self):
-        return len(self.data())
+        length=LogBasic.length(self)
+        scale=1+self.skip
+        length2=length/scale
+        if length%scale>0: length2+=1
+        return length2        
     def type(self):
-        return 'sc%d'%self.length()
-    def data(self):
-        return self.origin.getValues().getValues()
+        return 's<%d>'%self.length()
     def tick(self,dt):
-        for i,s in enumerate(self.data()):
+        data=self.data()
+        if self.skip>0:
+            data=[data[i] for i in range(0,len(data),self.skip+1)]
+        for i,s in enumerate(data):
             if s!=0: self.value[i]+=s
     def text(self):
-        r='<%s>'%(';'.join(['%d'%x for x in self.value]))
-        self.init()
-        return r
+        return ';'.join(['%d'%x for x in self.value])
+    def flush(self):
+        self.value[:]=0
 
 
 
 class Log(SimpleNode):
-    def __init__(self,network,name=None,filename=None,interval=0.1,tau=0.01):
+    def __init__(self,network,name=None,dir=None,filename='%(name)s-%(time)s.csv',interval=0.1,tau=0.01):
         if not isinstance(network,Network):
-            network=nef.Network(network)
+            network=Network(network)
         self.network=network
 
         if name is None: self.logname=self.network.network.name
-        self.filename=None
-        self.fileext='.csv'
-        if not filename is None: 
-            self.filename=filename
-            self.fileext=''
-
-        self.genheader=True
+        else: self.logname=name
         
-        self.tau=0.01
-        self.log_names=[]
-        self.logs={}
+        self.dir=dir
+        if not filename.endswith('.csv'): filename+='.csv'
+        self.filename_template=filename
         self.interval=interval
+        self.tau=tau
+
+        self.logs=[]
         SimpleNode.__init__(self,'Log')
         self.network.add(self)
     
     def init(self):
-        if self.filename is None: self.filename=self.make_filename()
-        self.file=None
-        for log in self.logs.values():
+        self.filename=self.make_filename()
+        for log in self.logs:
             log.init()
         self.next_time=0
-        self.genheader=True
+        self.wrote_header=False
 
     def make_filename(self):
         t=time.strftime('%Y%m%d-%H%M%S')
-        return '%s-%s'%(t,self.logname)
+        fn=self.filename_template%dict(time=t,name=self.logname)
+        if self.dir is not None: fn='%s/%s'%(self.dir,fn)
+        return fn
                     
     def tick(self):
         if len(self.logs)==0: return
-        if(self.genheader): self.write_header()
+        if not self.wrote_header: self.write_header()
         dt=self.t_end-self.t_start
-        for log in self.logs.values():
+        for log in self.logs:
             log.tick(dt)
         if self.t>=self.next_time:
             self.next_time+=self.interval
-            self.write_to_log()    
+            self.write_data()    
         
-    def add_spikes(self,source,name=None):    
+    def add_spikes(self,source,name=None,skip=0):    
         if name is None: name=source
-        self.log_names.append(name)   
-        if not source.endswith('.AXON'): source=source+'.AXON'     
-        origin=self.network.get(source)
-        self.logs[name]=LogSpikeCount(origin)
+        
+        node=self.network.get(source)
+        if isinstance(node,Origin):
+            origin=node
+        elif isinstance(node,NetworkArray):
+            origin=[n.getOrigin('AXON') for n in node.nodes]
+        else:    
+            origin=node.getOrigin('AXON')
+        self.logs.append(LogSpikeCount(name,origin,skip=skip))
         
     def add(self,source,name=None,tau=None):
         if name is None: name=source
         if tau is None: tau=self.tau
-        self.log_names.append(name)        
         origin=self.network.get(source,require_origin=True)
-        self.logs[name]=LogVector(origin,tau)
+        self.logs.append(LogVector(name,origin,tau))
 
     def add_vocab(self,source,vocab=None,name=None,tau=None,terms=None):
         if name is None: name=source
         if tau is None: tau=self.tau
-        self.log_names.append(name)        
         origin=self.network.get(source,require_origin=True)
         if vocab is None: 
             dim=origin.dimensions
             vocab=hrr.Vocabulary.defaults[dim]        
-        self.logs[name]=LogVocab(origin,tau=tau,vocab=vocab,terms=terms)
-        
+        self.logs.append(LogVocab(name,origin,tau=tau,vocab=vocab,terms=terms))
         
     def write_header(self):
-        fileobj=open(self.filename+self.fileext,'a')
-        fileobj.write('time,%s\n'%(','.join(self.log_names)))
-        types=[self.logs[n].type() for n in self.log_names]
-        #self.file.write('float,%s\n'%(','.join(types)))
-        fileobj.close()
-        self.genheader=False
+        f=open(self.filename,'a')
+        f.write('\n')
+        f.write('time,%s\n'%(','.join([log.name for log in self.logs])))
+        f.write('    ,%s\n'%(','.join([log.type() for log in self.logs])))
+        f.close()
+        self.wrote_header=True
         
         
-    def write_to_log(self):
-        fileobj=open(self.filename+self.fileext,'a')
-        data=[self.logs[n].text() for n in self.log_names]
-        fileobj.write('%1.3f,%s\n'%(self.t,','.join(data)))
-        fileobj.close()
+    def write_data(self):
+        data=[log.text() for log in self.logs]
+        self.write('%1.3f,%s\n'%(self.t,','.join(data)))
+        
+    def write(self,text):
+        f=open(self.filename,'a')
+        f.write(text)
+        f.close()
         
         
            
