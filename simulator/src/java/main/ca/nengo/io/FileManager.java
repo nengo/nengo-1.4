808b5ae44666a08a9bba18141803a0693865e44d
Trevor Bekolay <tbekolay@gmail.com>
2011-12-09 16:39:27 +0000

    More JavaDoc updates (ca.nengo.io)

--- simulator/src/java/main/ca/nengo/io/FileManager.java
+++ simulator/src/java/main/ca/nengo/io/FileManager.java
@@ -1,29 +1,29 @@
 /*
-The contents of this file are subject to the Mozilla Public License Version 1.1 
-(the "License"); you may not use this file except in compliance with the License. 
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.mozilla.org/MPL/
 
 Software distributed under the License is distributed on an "AS IS" basis, WITHOUT
-WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+WARRANTY OF ANY KIND, either express or implied. See the License for the specific
 language governing rights and limitations under the License.
 
-The Original Code is "FileManager.java". Description: 
-"Handles saving and loading of Node   
-  
+The Original Code is "FileManager.java". Description:
+"Handles saving and loading of Node
+
   TODO: a better job (this is a quick one)
-  TODO: is there any metadata to store? 
+  TODO: is there any metadata to store?
   TODO: test
-  
+
   @author Bryan Tripp"
 
 The Initial Developer of the Original Code is Bryan Tripp & Centre for Theoretical Neuroscience, University of Waterloo. Copyright (C) 2006-2008. All Rights Reserved.
 
-Alternatively, the contents of this file may be used under the terms of the GNU 
-Public License license (the GPL License), in which case the provisions of GPL 
-License are applicable  instead of those above. If you wish to allow use of your 
-version of this file only under the terms of the GPL License and not to allow 
-others to use your version of this file under the MPL, indicate your decision 
-by deleting the provisions above and replace  them with the notice and other 
+Alternatively, the contents of this file may be used under the terms of the GNU
+Public License license (the GPL License), in which case the provisions of GPL
+License are applicable  instead of those above. If you wish to allow use of your
+version of this file only under the terms of the GPL License and not to allow
+others to use your version of this file under the MPL, indicate your decision
+by deleting the provisions above and replace  them with the notice and other
 provisions required by the GPL License.  If you do not delete the provisions above,
 a recipient may use your version of this file under either the MPL or the GPL License.
 */
@@ -44,40 +44,59 @@ import ca.nengo.model.Node;
 import ca.nengo.util.TimeSeries;
 
 /**
- * Handles saving and loading of Node   
- * 
+ * Handles saving and loading of Node
+ *
  * TODO: a better job (this is a quick one)
- * TODO: is there any metadata to store? 
+ * TODO: is there any metadata to store?
  * TODO: test
- * 
+ *
  * @author Bryan Tripp
  */
 public class FileManager {
 
+	/**
+	 * Extension for serialized NEF networks
+	 */
 	public static final String ENSEMBLE_EXTENSION = "nef";
-	
+
 	private static File ourDefaultLocation = new File("./work");
 	static {
 		ourDefaultLocation.mkdirs();
 	}
-	
+
+	/**
+	 * @return Default location for saving files
+	 */
 	public static File getDefaultLocation() {
 		return ourDefaultLocation;
 	}
-	
+
+	/**
+	 * @param location Default location for saving files
+	 */
 	public static void setDefaultLocation(File location) {
 		ourDefaultLocation = location;
 	}
 
+	/**
+	 * @param node Node to serialize
+	 * @param destination File to save serialized Node in
+	 * @throws IOException if there's a problem writing to disk
+	 */
 	public void save(Node node, File destination) throws IOException {
 		saveObject(node, destination);
 	}
-	
+
+	/**
+	 * @param timeSeries TimeSeries to serialize
+	 * @param destination File to save serialized TimeSeries in
+	 * @throws IOException if there's a problem writing to disk
+	 */
 	public void save(TimeSeries timeSeries, File destination) throws IOException {
 		saveObject(timeSeries, destination);
 	}
-	
-	private static void saveObject(Object object, File destination) throws IOException {		
+
+	private static void saveObject(Object object, File destination) throws IOException {
 		FileOutputStream fos = new FileOutputStream(destination);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		oos.writeObject(object);
@@ -85,17 +104,24 @@ public class FileManager {
 		oos.close();
 		fos.close();
 	}
-	
+
+	/**
+	 * @param source Serialized file to load
+	 * @return Object represented by the serialized file
+	 * @throws IOException if there's a problem writing to disk
+	 * @throws ClassNotFoundException if the serialized file contains classes
+	 *   not known in this context
+	 */
 	public Object load(File source) throws IOException, ClassNotFoundException {
 		FileInputStream fis = new FileInputStream(source);
-		
+
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		Object return_obj = ois.readObject();
-		
+
 		ois.close();
 		fis.close();
-		
+
 		return return_obj;
 	}
-	
+
 }
--- simulator/src/java/main/ca/nengo/io/MatlabExporter.java
+++ simulator/src/java/main/ca/nengo/io/MatlabExporter.java
@@ -1,23 +1,23 @@
 /*
-The contents of this file are subject to the Mozilla Public License Version 1.1 
-(the "License"); you may not use this file except in compliance with the License. 
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.mozilla.org/MPL/
 
 Software distributed under the License is distributed on an "AS IS" basis, WITHOUT
-WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+WARRANTY OF ANY KIND, either express or implied. See the License for the specific
 language governing rights and limitations under the License.
 
-The Original Code is "MatlabExporter.java". Description: 
+The Original Code is "MatlabExporter.java". Description:
 "A tool for exporting data to Matlab .mat files"
 
 The Initial Developer of the Original Code is Bryan Tripp & Centre for Theoretical Neuroscience, University of Waterloo. Copyright (C) 2006-2008. All Rights Reserved.
 
-Alternatively, the contents of this file may be used under the terms of the GNU 
-Public License license (the GPL License), in which case the provisions of GPL 
-License are applicable  instead of those above. If you wish to allow use of your 
-version of this file only under the terms of the GPL License and not to allow 
-others to use your version of this file under the MPL, indicate your decision 
-by deleting the provisions above and replace  them with the notice and other 
+Alternatively, the contents of this file may be used under the terms of the GNU
+Public License license (the GPL License), in which case the provisions of GPL
+License are applicable  instead of those above. If you wish to allow use of your
+version of this file only under the terms of the GPL License and not to allow
+others to use your version of this file under the MPL, indicate your decision
+by deleting the provisions above and replace  them with the notice and other
 provisions required by the GPL License.  If you do not delete the provisions above,
 a recipient may use your version of this file under either the MPL or the GPL License.
 */
@@ -29,61 +29,64 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.jmatio.io.MatFileWriter;
-import com.jmatio.types.MLArray;
-import com.jmatio.types.MLDouble;
-
 import ca.nengo.plot.Plotter;
 import ca.nengo.util.MU;
 import ca.nengo.util.SpikePattern;
 import ca.nengo.util.TimeSeries;
 
+import com.jmatio.io.MatFileWriter;
+import com.jmatio.types.MLArray;
+import com.jmatio.types.MLDouble;
+
 /**
- * <p>A tool for exporting data to Matlab .mat files. Use like this:</p> 
- * 
+ * <p>A tool for exporting data to Matlab .mat files. Use like this:</p>
+ *
  * <p><code>
  * MatlabExport me = new MatlabExport();<br>
  * me.add("series1", series1);<br>
  * ... <br>
  * me.add("series1", series1);<br>
- * me.write(new File("c:\\foo.mat"));<br> 
+ * me.write(new File("c:\\foo.mat"));<br>
  * </code></p>
- *  
+ *
  * @author Bryan Tripp
  */
 public class MatlabExporter {
 
 	private Map<String, MLArray> myData;
-	
+
+	/**
+	 * Export data to Matlab .mat files
+	 */
 	public MatlabExporter() {
 		myData = new HashMap<String, MLArray>(10);
 	}
-	
+
 	/**
-	 * @param name Matlab variable name 
-	 * @param data Data to be stored in Matlab variable 
+	 * @param name Matlab variable name
+	 * @param data Data to be stored in Matlab variable
 	 */
 	public void add(String name, TimeSeries data) {
 		add(name+"_time", new float[][]{data.getTimes()});
 		add(name, data.getValues());
 	}
-	
+
 	/**
-	 * Filters TimeSeries data with given time constant (this is usually a good 
+	 * Filters TimeSeries data with given time constant (this is usually a good
 	 * idea for spike output, which is a sum of impulses).
-	 * 
+	 *
 	 * TODO: this filter is prohibitively slow for large datasets
-	 * 
-	 * @param name Matlab variable name 
-	 * @param data Data to be stored in Matlab variable 
+	 *
+	 * @param name Matlab variable name
+	 * @param data Data to be stored in Matlab variable
 	 * @param tau Time constant of filter to apply to data
 	 */
 	public void add(String name, TimeSeries data, float tau) {
 		TimeSeries filtered = Plotter.filter(data, tau);
 		add(name+"_time", new float[][]{filtered.getTimes()});
-		add(name+"_data", filtered.getValues());		
+		add(name+"_data", filtered.getValues());
 	}
-	
+
 	/**
 	 * @param name Matlab variable name
 	 * @param pattern Spike times for a group of neurons
@@ -93,9 +96,11 @@ public class MatlabExporter {
 		int maxSpikes = 0;
 		for (int i = 0; i < n; i++) {
 			float[] times = pattern.getSpikeTimes(i);
-			if (times.length > maxSpikes) maxSpikes = times.length;
+			if (times.length > maxSpikes) {
+                maxSpikes = times.length;
+            }
 		}
-		float[][] timesMatrix = new float[n][]; 
+		float[][] timesMatrix = new float[n][];
 		for (int i = 0; i < n; i++) {
 			timesMatrix[i] = new float[maxSpikes];
 			float[] times = pattern.getSpikeTimes(i);
@@ -103,7 +108,7 @@ public class MatlabExporter {
 		}
 		add(name, timesMatrix);
 	}
-	
+
 	/**
 	 * @param name Matlab variable name
 	 * @param data A matrix
@@ -113,14 +118,18 @@ public class MatlabExporter {
 			throw new IllegalArgumentException("Data must be a matrix (same number of columns in each row)");
 		}
 		name=makeVariableNameValid(name);
-		MLDouble mld = new MLDouble(name, MU.convert(data));	
+		MLDouble mld = new MLDouble(name, MU.convert(data));
 		myData.put(name, mld);
 	}
-	
+
+	/**
+	 * @param name original possibly invalid name
+	 * @return valid name that Matlab can use
+	 */
 	public static String makeVariableNameValid(String name) {
 		// replace all invalid characters with underscores
-		name=name.replaceAll("[^a-zA-Z0-9_]","_");		
-		
+		name=name.replaceAll("[^a-zA-Z0-9_]","_");
+
 		// make sure the variable name starts with a letter
 		if (name.length()==0) {
 			name="data";
@@ -129,19 +138,19 @@ public class MatlabExporter {
 		}
 		return name;
 	}
-	
+
 	/**
-	 * Clears all variables 
+	 * Clears all variables
 	 */
 	public void removeAll() {
 		myData.clear();
 	}
-		
+
 	/**
-	 * Writes to given destination the data that have been added to this exporter. 
-	 * 
+	 * Writes to given destination the data that have been added to this exporter.
+	 *
 	 * @param destination File to which data are to be written (should have extension .mat)
-	 * @throws IOException 
+	 * @throws IOException if there's a problem writing to disk
 	 */
 	public void write(File destination) throws IOException {
 		new MatFileWriter(destination, myData.values());
