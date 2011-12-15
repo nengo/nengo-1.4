9c8b2d425a43897ab06c0adce8cdfb5e892b2fab
Trevor Bekolay <tbekolay@gmail.com>
2011-12-15 22:50:39 +0000

    Added to dummy files.

--- simulator-ui/docs/dummy/ca/nengo/model/plasticity/impl/__init__.py
+++ simulator-ui/docs/dummy/ca/nengo/model/plasticity/impl/__init__.py
@@ -1,7 +1,2 @@
-class ErrorLearningFunction: pass
-class InSpikeErrorFunction: pass
-class OutSpikeErrorFunction: pass
-class RealPlasticityRule: pass
-class SpikePlasticityRule: pass
-class RealPlasticityTermination: pass
-class SpikePlasticityTermination: pass
+class PESTermination: pass
+class STDPTermination: pass
