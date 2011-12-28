027406a0580b97fa595db22f78e7e39bd72a5e74
Trevor Bekolay <tbekolay@gmail.com>
2011-12-28 00:23:32 +0000

    More JavaDoc updates (ca.nengo.ui.brainView)

--- simulator-ui/src/java/main/ca/nengo/ui/brainView/BrainData.java
+++ simulator-ui/src/java/main/ca/nengo/ui/brainView/BrainData.java
@@ -1,26 +1,26 @@
 /*
-The contents of this file are subject to the Mozilla Public License Version 1.1 
-(the "License"); you may not use this file except in compliance with the License. 
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.mozilla.org/MPL/
 
 Software distributed under the License is distributed on an "AS IS" basis, WITHOUT
-WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+WARRANTY OF ANY KIND, either express or implied. See the License for the specific
 language governing rights and limitations under the License.
 
-The Original Code is "BrainData.java". Description: 
+The Original Code is "BrainData.java". Description:
 ""
 
 The Initial Developer of the Original Code is Bryan Tripp & Centre for Theoretical Neuroscience, University of Waterloo. Copyright (C) 2006-2008. All Rights Reserved.
 
-Alternatively, the contents of this file may be used under the terms of the GNU 
-Public License license (the GPL License), in which case the provisions of GPL 
-License are applicable  instead of those above. If you wish to allow use of your 
-version of this file only under the terms of the GPL License and not to allow 
-others to use your version of this file under the MPL, indicate your decision 
-by deleting the provisions above and replace  them with the notice and other 
+Alternatively, the contents of this file may be used under the terms of the GNU
+Public License license (the GPL License), in which case the provisions of GPL
+License are applicable  instead of those above. If you wish to allow use of your
+version of this file only under the terms of the GPL License and not to allow
+others to use your version of this file under the MPL, indicate your decision
+by deleting the provisions above and replace  them with the notice and other
 provisions required by the GPL License.  If you do not delete the provisions above,
 a recipient may use your version of this file under either the MPL or the GPL License.
-*/
+ */
 
 package ca.nengo.ui.brainView;
 
@@ -35,116 +35,157 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * TODO
+ * 
+ * @author TODO
+ */
 public class BrainData {
-	public static String DATA_FILE_NAME = "t1_icbm_normal_1mm_pn3_rf20.rawb";
-	public static int X_DIMENSIONS = 181;
-	public static int Y_DIMENSIONS = 217;
-	public static int Z_DIMENSIONS = 181;
 
-	public static int X_START = 72;
-	public static int Y_START = 126;
-	public static int Z_START = 90;
+    /**
+     * TODO
+     */
+    public static String DATA_FILE_NAME = "t1_icbm_normal_1mm_pn3_rf20.rawb";
 
-	public static String DATA_FOLDER = "data";
+    /**
+     * TODO
+     */
+    public static int X_DIMENSIONS = 181;
 
-	static File dataFile = new File(DATA_FOLDER, DATA_FILE_NAME);
+    /**
+     * TODO
+     */
+    public static int Y_DIMENSIONS = 217;
 
-	private static final byte[][][] VOXEL_DATA = new byte[Z_DIMENSIONS][Y_DIMENSIONS][X_DIMENSIONS];
+    /**
+     * TODO
+     */
+    public static int Z_DIMENSIONS = 181;
 
-	private static boolean fileProcessed = false;
+    /**
+     * TODO
+     */
+    public static int X_START = 72;
 
-	public static void initVoxelData() {
-		processFile();
+    /**
+     * TODO
+     */
+    public static int Y_START = 126;
 
-	}
+    /**
+     * TODO
+     */
+    public static int Z_START = 90;
 
-	private static void processFile() {
-		if (fileProcessed) {
-			return;
-		}
-		fileProcessed = true;
+    /**
+     * TODO
+     */
+    public static String DATA_FOLDER = "data";
 
-		try {
-			FileInputStream fileStream = new FileInputStream(dataFile);
+    static File dataFile = new File(DATA_FOLDER, DATA_FILE_NAME);
 
-			for (int zIndex = 0; zIndex < Z_DIMENSIONS; zIndex++) {
-				for (int yIndex = 0; yIndex < Y_DIMENSIONS; yIndex++) {
-					fileStream.read(VOXEL_DATA[zIndex][yIndex]);
+    private static final byte[][][] VOXEL_DATA = new byte[Z_DIMENSIONS][Y_DIMENSIONS][X_DIMENSIONS];
 
-				}
-			}
+    private static boolean fileProcessed = false;
 
-			if (fileStream.available() != 0) {
-				throw new IOException(
-						"File size incorrect, does not match data dimensions");
-			}
-			fileStream.close();
+    /**
+     * TODO
+     */
+    public static void initVoxelData() {
+        processFile();
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    private static void processFile() {
+        if (fileProcessed) {
+            return;
+        }
+        fileProcessed = true;
 
-	public static void main(String[] args) {
+        try {
+            FileInputStream fileStream = new FileInputStream(dataFile);
 
-		MyCanvas canvas = new MyCanvas();
-		Frame frame = new Frame("BrainView");
-		frame.add(canvas);
-		frame.setSize(300, 200);
-		frame.setVisible(true);
+            for (int zIndex = 0; zIndex < Z_DIMENSIONS; zIndex++) {
+                for (int yIndex = 0; yIndex < Y_DIMENSIONS; yIndex++) {
+                    fileStream.read(VOXEL_DATA[zIndex][yIndex]);
 
-		for (int i = -50; i < 50; i++) {
+                }
+            }
 
-			canvas.setImagePosition(i);
-			try {
-				Thread.sleep(25);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+            if (fileStream.available() != 0) {
+                throw new IOException(
+                        "File size incorrect, does not match data dimensions");
+            }
+            fileStream.close();
 
-	}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	static class MyCanvas extends Canvas {
+    }
 
-		private static final long serialVersionUID = 1L;
+    /**
+     * @param args TODO
+     */
+    public static void main(String[] args) {
 
-		BrainTopImage image;
+        MyCanvas canvas = new MyCanvas();
+        Frame frame = new Frame("BrainView");
+        frame.add(canvas);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
 
-		public void setImagePosition(int zCoord) {
-			image.setCoord(zCoord);
-			repaint();
-		}
+        for (int i = -50; i < 50; i++) {
 
-		MyCanvas() {
+            canvas.setImagePosition(i);
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-			image = new BrainTopImage();
+    }
 
-			// Add a listener for resize events
-			addComponentListener(new ComponentAdapter() {
-				// This method is called when the component's size changes
-				public void componentResized(ComponentEvent evt) {
-					Component c = (Component) evt.getSource();
+    static class MyCanvas extends Canvas {
 
-					// Regenerate the image
-					c.repaint();
+        private static final long serialVersionUID = 1L;
 
-				}
-			});
-		}
+        BrainTopImage image;
 
-		public void paint(Graphics g) {
-			if (image != null) {
-				g.drawImage(image, 0, 0, null);
-			}
-		}
-	}
+        public void setImagePosition(int zCoord) {
+            image.setCoord(zCoord);
+            repaint();
+        }
 
-	protected static byte[][][] getVoxelData() {
-		initVoxelData();
-		return VOXEL_DATA;
-	}
+        MyCanvas() {
+
+            image = new BrainTopImage();
+
+            // Add a listener for resize events
+            addComponentListener(new ComponentAdapter() {
+                // This method is called when the component's size changes
+                public void componentResized(ComponentEvent evt) {
+                    Component c = (Component) evt.getSource();
+
+                    // Regenerate the image
+                    c.repaint();
+
+                }
+            });
+        }
+
+        public void paint(Graphics g) {
+            if (image != null) {
+                g.drawImage(image, 0, 0, null);
+            }
+        }
+    }
+
+    protected static byte[][][] getVoxelData() {
+        initVoxelData();
+        return VOXEL_DATA;
+    }
 }
--- simulator-ui/src/java/main/ca/nengo/ui/brainView/BrainViewer.java
+++ simulator-ui/src/java/main/ca/nengo/ui/brainView/BrainViewer.java
@@ -1,26 +1,26 @@
 /*
-The contents of this file are subject to the Mozilla Public License Version 1.1 
-(the "License"); you may not use this file except in compliance with the License. 
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://www.mozilla.org/MPL/
 
 Software distributed under the License is distributed on an "AS IS" basis, WITHOUT
-WARRANTY OF ANY KIND, either express or implied. See the License for the specific 
+WARRANTY OF ANY KIND, either express or implied. See the License for the specific
 language governing rights and limitations under the License.
 
-The Original Code is "BrainViewer.java". Description: 
+The Original Code is "BrainViewer.java". Description:
 ""
 
 The Initial Developer of the Original Code is Bryan Tripp & Centre for Theoretical Neuroscience, University of Waterloo. Copyright (C) 2006-2008. All Rights Reserved.
 
-Alternatively, the contents of this file may be used under the terms of the GNU 
-Public License license (the GPL License), in which case the provisions of GPL 
-License are applicable  instead of those above. If you wish to allow use of your 
-version of this file only under the terms of the GPL License and not to allow 
-others to use your version of this file under the MPL, indicate your decision 
-by deleting the provisions above and replace  them with the notice and other 
+Alternatively, the contents of this file may be used under the terms of the GNU
+Public License license (the GPL License), in which case the provisions of GPL
+License are applicable  instead of those above. If you wish to allow use of your
+version of this file only under the terms of the GPL License and not to allow
+others to use your version of this file under the MPL, indicate your decision
+by deleting the provisions above and replace  them with the notice and other
 provisions required by the GPL License.  If you do not delete the provisions above,
 a recipient may use your version of this file under either the MPL or the GPL License.
-*/
+ */
 
 package ca.nengo.ui.brainView;
 
@@ -35,152 +35,161 @@ import ca.nengo.ui.lib.world.piccolo.primitives.Text;
 import edu.umd.cs.piccolo.event.PDragSequenceEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 
+/**
+ * TODO
+ * 
+ * @author TODO
+ */
 public class BrainViewer extends WorldImpl {
 
-	private static final long serialVersionUID = 1L;
+    AbstractBrainImage2D topView, sideView, frontView;
 
-	AbstractBrainImage2D topView, sideView, frontView;
+    /**
+     * TODO
+     */
+    public BrainViewer() {
+        super("Brain View", createGround());
 
-	public BrainViewer() {
-		super("Brain View", createGround());
+        setStatusBarHandler(null);
+        init();
 
-		setStatusBarHandler(null);
-		init();
+        // getSky().setScale(2);
+        // setBounds(parentToLocal(getFullBounds()));
+        // addInputEventListener(new EventConsumer());
 
-		// getSky().setScale(2);
-		// setBounds(parentToLocal(getFullBounds()));
-		// addInputEventListener(new EventConsumer());
+    }
 
-	}
+    private void init() {
+        sideView = new BrainSideImage();
+        frontView = new BrainFrontImage();
 
-	private void init() {
-		sideView = new BrainSideImage();
-		frontView = new BrainFrontImage();
+        topView = new BrainTopImage();
 
-		topView = new BrainTopImage();
+        getGround().addChild(new BrainImageWrapper(sideView));
+        getGround().addChild(new BrainImageWrapper(frontView));
+        getGround().addChild(new BrainImageWrapper(topView));
 
-		getGround().addChild(new BrainImageWrapper(sideView));
-		getGround().addChild(new BrainImageWrapper(frontView));
-		getGround().addChild(new BrainImageWrapper(topView));
+    }
 
-	}
+    /**
+     * @return TODO
+     */
+    public int getZCoord() {
+        return topView.getCoord();
+    }
 
-	public int getZCoord() {
-		return topView.getCoord();
-	}
+    /**
+     * @return TODO
+     */
+    public int getYCoord() {
+        return frontView.getCoord();
+    }
 
-	public int getYCoord() {
-		return frontView.getCoord();
-	}
+    private static WorldGroundImpl createGround() {
+        return new WorldGroundImpl() {
 
-	private static WorldGroundImpl createGround() {
-		return new WorldGroundImpl() {
+            @Override
+            public void layoutChildren() {
 
-			private static final long serialVersionUID = 1L;
+                int x = 0;
+                double maxHeight = 0;
 
-			@Override
-			public void layoutChildren() {
+                for (WorldObject wo : getChildren()) {
 
-				int x = 0;
-				double maxHeight = 0;
+                    if (wo.getHeight() > maxHeight) {
+                        maxHeight = wo.getHeight();
+                    }
 
-				for (WorldObject wo : getChildren()) {
+                }
 
-					if (wo.getHeight() > maxHeight) {
-						maxHeight = wo.getHeight();
-					}
+                for (WorldObject wo : getChildren()) {
 
-				}
+                    wo.setOffset(x, maxHeight - wo.getHeight());
+                    x += wo.getWidth() + 10;
 
-				for (WorldObject wo : getChildren()) {
-
-					wo.setOffset(x, maxHeight - wo.getHeight());
-					x += wo.getWidth() + 10;
-
-				}
-			}
-		};
-	}
+                }
+            }
+        };
+    }
 }
 
 class BrainImageWrapper extends WorldObjectImpl {
 
-	private static final long serialVersionUID = 1L;
-	AbstractBrainImage2D myBrainImage;
-	Text myLabel;
+    AbstractBrainImage2D myBrainImage;
+    Text myLabel;
 
-	public BrainImageWrapper(AbstractBrainImage2D brainImage) {
-		super();
-		myBrainImage = brainImage;
-		addChild(new WorldObjectImpl(new PXImage(brainImage)));
+    public BrainImageWrapper(AbstractBrainImage2D brainImage) {
+        super();
+        myBrainImage = brainImage;
+        addChild(new WorldObjectImpl(new PXImage(brainImage)));
 
-		myLabel = new Text();
-		addChild(myLabel);
-		updateLabel();
+        myLabel = new Text();
+        addChild(myLabel);
+        updateLabel();
 
-		addInputEventListener(new EventConsumer());
-		addInputEventListener(new BrainImageMouseHandler());
+        addInputEventListener(new EventConsumer());
+        addInputEventListener(new BrainImageMouseHandler());
 
-		layoutChildren();
-		setBounds(parentToLocal(getFullBounds()));
-		// addChild(new Border(this, Style.COLOR_FOREGROUND));
+        layoutChildren();
+        setBounds(parentToLocal(getFullBounds()));
+        // addChild(new Border(this, Style.COLOR_FOREGROUND));
 
-	}
+    }
 
-	private void updateLabel() {
-		myLabel.setText(myBrainImage.getViewName() + " (" + myBrainImage.getCoordName()
-				+ " coord: " + myBrainImage.getCoord() + ")");
-	}
+    private void updateLabel() {
+        myLabel.setText(myBrainImage.getViewName() + " (" + myBrainImage.getCoordName()
+                + " coord: " + myBrainImage.getCoord() + ")");
+    }
 
-	@Override
-	public void layoutChildren() {
-		super.layoutChildren();
+    @Override
+    public void layoutChildren() {
+        super.layoutChildren();
 
-		myLabel.setOffset(0, myBrainImage.getHeight() + 10);
-	}
+        myLabel.setOffset(0, myBrainImage.getHeight() + 10);
+    }
 
-	class BrainImageMouseHandler extends PDragSequenceEventHandler {
+    class BrainImageMouseHandler extends PDragSequenceEventHandler {
 
-		double roundingError = 0;
+        double roundingError = 0;
 
-		@Override
-		protected void dragActivityStep(PInputEvent aEvent) {
+        @Override
+        protected void dragActivityStep(PInputEvent aEvent) {
 
-			double dx = (aEvent.getCanvasPosition().getX() - getMousePressedCanvasPoint().getX()) / 30;
-			int dxInteger = (int) dx;
+            double dx = (aEvent.getCanvasPosition().getX() - getMousePressedCanvasPoint().getX()) / 30;
+            int dxInteger = (int) dx;
 
-			roundingError += dx - (int) dx;
+            roundingError += dx - (int) dx;
 
-			if (roundingError > 1) {
-				dxInteger++;
-				roundingError--;
-			} else if (roundingError < -1) {
-				roundingError++;
-				dxInteger--;
-			}
+            if (roundingError > 1) {
+                dxInteger++;
+                roundingError--;
+            } else if (roundingError < -1) {
+                roundingError++;
+                dxInteger--;
+            }
 
-			myBrainImage.setCoord(myBrainImage.getCoord() + dxInteger);
-			updateLabel();
-			repaint();
+            myBrainImage.setCoord(myBrainImage.getCoord() + dxInteger);
+            updateLabel();
+            repaint();
 
-		}
-	}
+        }
+    }
 }
 
 class BrainViewStatusHandler extends AbstractStatusHandler {
 
-	public BrainViewStatusHandler(BrainViewer world) {
-		super(world);
-	}
+    public BrainViewStatusHandler(BrainViewer world) {
+        super(world);
+    }
 
-	@Override
-	protected String getStatusMessage(PInputEvent event) {
-		return "Z Coord: " + getWorld().getZCoord();
+    @Override
+    protected String getStatusMessage(PInputEvent event) {
+        return "Z Coord: " + getWorld().getZCoord();
 
-	}
+    }
 
-	@Override
-	protected BrainViewer getWorld() {
-		return (BrainViewer) super.getWorld();
-	}
+    @Override
+    protected BrainViewer getWorld() {
+        return (BrainViewer) super.getWorld();
+    }
 }
