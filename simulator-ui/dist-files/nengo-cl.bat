fc4f4217a8bdb955888e089a48fe8f039aef2335
Eric Crawford <e2crawfo@uwaterloo.ca>
2012-02-15 18:09:23 +0000

    Added the jgrapht jar to the dist-files that run nengo.

--- simulator-ui/dist-files/nengo-cl.bat
+++ simulator-ui/dist-files/nengo-cl.bat
@@ -1,7 +1,7 @@
 @echo off
 IF [%1]==[] (
-    java -Xms100m -Xmx800m -cp .;nengo-BUILDNUMBER.jar;lib/Blas.jar;lib/colt.jar;lib/commons-collections-3.2.jar;lib/formsrt.jar;lib/Jama-1.0.2.jar;lib/jcommon-1.0.0.jar;lib/jfreechart-1.0.1.jar;lib/jmatio.jar;lib/jung-1.7.6.jar;lib/jython.jar;lib/log4j-1.2.16.jar;piccolo.jar;lib/piccolox.jar;lib/qdox-1.6.3.jar;lib/ssj.jar org.python.util.jython -i ./python/startup_cl.py
+    java -Xms100m -Xmx800m -cp .;nengo-BUILDNUMBER.jar;lib/Blas.jar;lib/colt.jar;lib/commons-collections-3.2.jar;lib/formsrt.jar;lib/Jama-1.0.2.jar;lib/jcommon-1.0.0.jar;lib/jfreechart-1.0.1.jar;lib/jmatio.jar;lib/jung-1.7.6.jar;lib/jython.jar;lib/log4j-1.2.16.jar;piccolo.jar;lib/piccolox.jar;lib/qdox-1.6.3.jar;lib/ssj.jar;lib/jgrapht-jdk1.6.jar org.python.util.jython -i ./python/startup_cl.py
 ) ELSE (
-    java -Xms100m -Xmx800m -cp .;nengo-BUILDNUMBER.jar;lib/Blas.jar;lib/colt.jar;lib/commons-collections-3.2.jar;lib/formsrt.jar;lib/Jama-1.0.2.jar;lib/jcommon-1.0.0.jar;lib/jfreechart-1.0.1.jar;lib/jmatio.jar;lib/jung-1.7.6.jar;lib/jython.jar;lib/log4j-1.2.16.jar;piccolo.jar;lib/piccolox.jar;lib/qdox-1.6.3.jar;lib/ssj.jar org.python.util.jython -c "execfile('./python/startup_cl.py'); execfile('%1')"
+    java -Xms100m -Xmx800m -cp .;nengo-BUILDNUMBER.jar;lib/Blas.jar;lib/colt.jar;lib/commons-collections-3.2.jar;lib/formsrt.jar;lib/Jama-1.0.2.jar;lib/jcommon-1.0.0.jar;lib/jfreechart-1.0.1.jar;lib/jmatio.jar;lib/jung-1.7.6.jar;lib/jython.jar;lib/log4j-1.2.16.jar;piccolo.jar;lib/piccolox.jar;lib/qdox-1.6.3.jar;lib/ssj.jar;lib/jgrapht-jdk1.6.jar org.python.util.jython -c "execfile('./python/startup_cl.py'); execfile('%1')"
 )
 
