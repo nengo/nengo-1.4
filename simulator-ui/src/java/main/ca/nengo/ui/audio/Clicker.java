6e05902b7c6c5e729e477e663007dec6b216c8f3
Terry Stewart <terry.stewart@gmail.com>
2012-02-21 23:01:13 +0000

    - changed clicking algorithm to hopefully work better across machines

--- simulator-ui/src/java/main/ca/nengo/ui/audio/Clicker.java
+++ simulator-ui/src/java/main/ca/nengo/ui/audio/Clicker.java
@@ -8,17 +8,18 @@ public class Clicker {
 	boolean started=false;
 	SourceDataLine line;
 	byte[] value=new byte[bufferSize];
+	int targetBytesAvailable=5000;
 
-	private static final int bufferSize=400;
-	private static final int Hz=14400;
+	private static final int bufferSize=1000;
+	private static final int Hz=44100;
 	
 	public Clicker() {		
 	}
 	
 	public boolean start() {
 		if (started) return false;
-		AudioFormat format=new AudioFormat(Hz,8,1,false,false);
-		DataLine.Info info=new DataLine.Info(SourceDataLine.class, format,400);
+		AudioFormat format=new AudioFormat(Hz,8,1,false,true);
+		DataLine.Info info=new DataLine.Info(SourceDataLine.class, format,bufferSize);
 		if (!AudioSystem.isLineSupported(info)) return false;
 		
 		try {
@@ -33,8 +34,30 @@ public class Clicker {
 		return true;
 	}
 	
+	public void setTargetBytesAvailable(int value) {
+		targetBytesAvailable=value;
+	}
+	
+	public int getTargetBytesAvailable() {
+		return targetBytesAvailable;
+	}
+	
+	
+	int steps;
 	public void set(byte b) {
 		int avail=line.available();
+		
+		if (avail<targetBytesAvailable) {
+			steps-=1;
+		} else {
+			steps+=1;
+		}
+		if (steps<2) steps=2;
+		if (steps>=bufferSize) steps=bufferSize-1;
+		
+		//steps=500;
+
+		/*
 		if (avail<=0) return;
 		int steps;
 		
@@ -45,20 +68,21 @@ public class Clicker {
 		} else {
 			steps=(avail-100)/7+1;
 		}
-		
+		*/
 		
 		/*
 		steps=avail;
 		if (steps<0) steps=0;
 		if (steps>bufferSize) steps=bufferSize;
 		*/
-		//System.err.println("steps: "+steps);
+		//System.err.println("steps: "+steps+"  avail: "+avail);
 		
 		
-		value[bufferSize-1]=b;
+		//value[bufferSize-1]=b;
+		value[0]=b;
+		line.write(value,0,(int)steps);
 		
-		
-		line.write(value,(int)(bufferSize-steps),(int)steps);
+		//line.write(value,(int)(bufferSize-steps),(int)steps);
 	}
 	
 	public void stop() {
