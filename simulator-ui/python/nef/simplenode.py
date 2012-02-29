30cb047972c474d0e0063cfe12417b1a68fc7631
Terry Stewart <terry.stewart@gmail.com>
2012-02-29 00:34:17 +0000

    - fixed horrible bug in SimpleNode filtering code

--- simulator-ui/python/nef/simplenode.py
+++ simulator-ui/python/nef/simplenode.py
@@ -49,10 +49,12 @@ class SimpleTermination(Termination):
         if self.tau<dt or dt==0 or self.tau<=0:
             self._filtered_values=v
         else:    
-            decay=math.exp(dt/self.tau)
+            decay=math.exp(-dt/self.tau)
             for i in range(self._dimensions):
                 x=self._filtered_values[i]
                 self._filtered_values[i]=x*decay+v[i]*(1-decay)
+                #if i==10:
+                #    print start,end,dt,self.tau,decay,x,v[i],self._filtered_values[i]    
             
                 #self._filtered_values[i]*=(1.0-dt/self.tau)
                 #self._filtered_values[i]+=v[i]*dt/self.tau
