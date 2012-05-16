4f27e6482adc6ce3aec24cbcaf031c8f6dbab8fd
Terry Stewart <terry.stewart@gmail.com>
2012-05-16 18:53:02 +0000

    - added nef.run and nef.run_with commands to support running experiments

--- simulator-ui/python/nef/__init__.py
+++ simulator-ui/python/nef/__init__.py
@@ -9,5 +9,6 @@ from simplenode import SimpleNode
 from fsmnode import FSMNode
 from w2fnode import WriteToFileNode
 from log import Log
+from stats.runner import run,run_with
 
-__all__=['Network','SimpleNode','FSMNode','WriteToFileNode','Log']
+__all__=['Network','SimpleNode','FSMNode','WriteToFileNode','Log','run','run_with']
