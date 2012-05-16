c35b8b7c08c56781a9aa4ec7f026d5e8416672ad
Terry Stewart <terry.stewart@gmail.com>
2012-05-16 18:54:49 +0000

    - moved LogReader into stats/reader.py - added run_in_nengo command to support iPython notebook - hopefully this whole file can be phased out eventually

--- simulator-ui/python/datatools.py
+++ simulator-ui/python/datatools.py
@@ -1,75 +1,30 @@
 from __future__ import with_statement
-import os
+import os, re, time
 
 try:
-    import numpy
-    has_numpy=True
+    import numpy as np
 except:
-    has_numpy=False
+    import numeric as np
 
-try:
-    import numeric
-    has_numeric=True
-except:
-    has_numeric=False
+    
+last_run_in_nengo=None
 
-class LogReader:
-    def __init__(self,filename='',dir=None):
-        if dir is None: dir='.'
-        self.dir=dir
-        self.filename=self.find_file(filename)
-        if self.filename is not None:
-            self.read_header()
-    def find_file(self,filename):
-        files=os.listdir(self.dir)
-        files=[x for x in files if x.endswith('.csv') and x.startswith(filename)]
-        times=[os.path.getmtime(os.path.join(self.dir,x)) for x in files]
-        if len(times)==0:
-            print 'No log files found in "%s/%s*"'%(self.dir,self.filename)
-            return None
-        return files[times.index(max(times))]
-    def read_header(self):
-        with open(os.path.join(self.dir,self.filename)) as f:
-            for row in f:
-                row=row.strip()
-                if len(row)==0 or row.startswith('#'): 
-                    continue
-                self.header=row.split(',')
-                break
-    def parse(self,text):
-        data=text.split(';')
-        return [float(x) for x in data]
-    def __getattr__(self,key):
-        return self.get(key)    
-    def __getitem__(self,key):
-        return self.get(key)    
-    def get_index_for_time(self,time):
-        t=self.get('time')
-        index=0
-        while len(t)>index and t[index][0]<time:
-            index+=1        
-        return index    
-        
-    def get(self,name,time=None):
-        data=[]
-        done_header=False
-        index=self.header.index(name)
-        with open(os.path.join(self.dir,self.filename)) as f:
-            for row in f:
-                row=row.strip()
-                if len(row)==0 or row.startswith('#'): 
-                    continue
-                if not done_header:
-                    done_header=True
-                    continue
-                data.append(self.parse(row.split(',')[index]))
-        if time is not None:
-            if isinstance(time,(float,int)):
-                data=data[self.get_index_for_time(time)]
-            else:
-                data=data[self.get_index_for_time(time[0]):self.get_index_for_time(time[1])]        
-        if has_numpy: 
-            data=numpy.array(data)        
-        elif has_numeric:
-            data=numeric.array(data)    
-        return data
+def run_in_nengo(code):
+    global last_run_in_nengo
+    m=re.search('nef\.Network[(](.+)[),]',code)
+    if m is None:
+        fn='experiment-'+time.strftime('%Y%m%d-%H%M%S')+'.py'
+    else:
+        fn='experiment-%s.py'%(m.group(1).replace(' ','_').replace('"','').replace("'",''))
+    last_run_in_nengo=fn    
+    f=open(fn,'w')    
+    f.write(code)
+    f.close()
+    os.system('./nengo-cl '+fn)
+    print 'finished running',fn
+    
+def rms(x):
+    x=np.ravel(x)    
+    return np.sqrt(sum(x*x)/len(x))
+    
+   
