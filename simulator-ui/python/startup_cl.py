7b1a2a8384aad25e96714ffaa2ccbeec43659a8b
Terry Stewart <terry.stewart@gmail.com>
2012-05-26 18:14:46 +0000

    - added startup message for nengo-cl when run without arguments

--- simulator-ui/python/startup_cl.py
+++ simulator-ui/python/startup_cl.py
@@ -8,6 +8,27 @@ if len(sys.argv)>0:
     # run the script indicated on the command line
     execfile(sys.argv[0])
 else:
+    print """
+Welcome to Nengo!  <http://nengo.ca>
+    
+Nengo is an open-source neural simulator for large-scale systems, developed at
+the University of Waterloo by the Centre for Theoretical Neuroscience.
+
+This is the command-line interface, useful for quickly running scripts and
+for embedding Nengo within other research tools.  There is also a graphical
+user interface available by running 'nengo' (rather than 'nengo-cl', which
+is this program).
+
+The scripting language is Python.  You can run .py scripts as follows:
+    execfile('directory/script.py')
+You can also create models manually and even run the interactive plots viewer:
+    import nef
+    net=nef.Network('My Network')
+    input=net.make_input('input',[0])
+    A=net.make('A',neurons=50,dimensions=1)
+    net.connect(input,A)
+    net.view()
+"""
     # start an interactive console
     sys.argv=['-i']
     import org.python.util
