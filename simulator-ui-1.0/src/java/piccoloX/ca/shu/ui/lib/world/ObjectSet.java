67e1f740ede3eb60ecca37885504805bb7dfa482
Trevor Bekolay <tbekolay@gmail.com>
2012-07-10 14:12:06 -0400

    Removed old 1.0 versions

--- simulator-ui-1.0/src/java/piccoloX/ca/shu/ui/lib/world/ObjectSet.java
+++
@@ -1,9 +0,0 @@
-package ca.shu.ui.lib.world;
-
-import java.util.HashSet;
-
-public class ObjectSet<T extends WorldObject> extends HashSet<T> {
-
-	private static final long serialVersionUID = 1L;
-
-}
