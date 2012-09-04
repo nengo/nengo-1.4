b128030a93756af69ffda49e4f32e24994b8fcd0
Chris <celiasmi@dyn-129-97-178-254.dynamic.uwaterloo.ca>
2012-09-04 09:19:05 -0400

    final tutorial build a brain update

--- simulator-ui/dist-files/Building a Brain/chapter8/2D_decision_integrator.py
+++ simulator-ui/dist-files/Building a Brain/chapter8/2D_decision_integrator.py
@@ -5,11 +5,11 @@ input=net.make_input('input',[0,0])
 
 MT=net.make('MT',100,2, noise=10) 
 LIP=net.make('LIP',200,2, noise=10) 
-LIPT=net.make('LIP Threshold',100,2,intercept=(.3,1),noise=10)
+output=net.make('output',100,2,intercept=(.3,1),noise=10)
 
 net.connect(input,MT,weight=1,pstc=0.1)  
 net.connect(MT,LIP,weight=.1, pstc=.01) 
 net.connect(LIP,LIP,pstc=0.1)  
-net.connect(LIP,LIPT,pstc=.01)
+net.connect(LIP,output,pstc=.01)
 
 net.add_to_nengo()
