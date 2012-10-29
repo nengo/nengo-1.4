57825739d6198b73c92675aba908facd1e1fd107
travis dewolf <travis.dewolf@gmail.com>
2012-10-29 15:49:33 -0400

    Fixes issue #192

--- simulator-ui/python/nef/templates/thalamus.py
+++ simulator-ui/python/nef/templates/thalamus.py
@@ -18,8 +18,21 @@ def test_params(net,p):
     except:
         pass
 
-def make(net,name='Network Array', neurons=50, D=2, useQuick=True):
+def make(net,name='Network Array', neurons=50, D=2, inhib_scale=3, tau_inhib=.005, useQuick=True):
     thal = net.make_array(name, neurons, D, max_rate=(100,300), intercept=(-1, 0), radius=1, encoders=[[1]], quick=useQuick)    
+
+    # setup inhibitory scaling matrix
+    inhib_scaling_matrix = [[0]*D for i in range(D)]
+    for i in range(D):
+        inhib_scaling_matrix[i][i] = -inhib_scale
+    # setup inhibitory matrix
+    inhib_matrix = []
+    for i in range(D):
+        inhib_matrix_part = [[inhib_scaling_matrix[i]] * neurons]
+        inhib_matrix.append(inhib_matrix_part[0])
+
+    thal.addTermination('bg_input', inhib_matrix, tau_inhib, False)
+
     def addOne(x):
         return [x[0]+1]            
     net.connect(thal, None, func=addOne, origin_name='xBiased', create_projection=False)
