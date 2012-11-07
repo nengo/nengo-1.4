aa4e3e152a4b86c1d04fbaa5ab0d9de0bdf7d285
James Bergstra <james.bergstra@gmail.com>
2012-11-07 16:47:36 -0500

    ENH: rebuild nengo prints what it did

--- rebuild_nengo_current.sh
+++ rebuild_nengo_current.sh
@@ -10,6 +10,8 @@
 set -e
 
 CURRENT="$(ls -t | grep nengo- | grep -v .zip | head -n 1)"
+echo 'If you see "BUILD FAILED" above and you are worried... relax!' "It's normal."
+echo "Moving right along..."
 rm -f nengo-current
 ln -s "$CURRENT" nengo-current
 NENGO_GIT_ROOT="$PWD"
@@ -17,3 +19,4 @@ NENGO_GIT_ROOT="$PWD"
 ( cd nengo-current && chmod +x nengo-cl external/ps* )
 ( cd nengo-current && mv python python_dontuse && ln -s "$NENGO_GIT_ROOT/simulator-ui/python" )
 
+echo "Nengo has been compiled to ./$CURRENT and symlinked as ./nengo-current"
