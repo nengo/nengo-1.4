3bc7e1415e39629f5f6244f1996361daac04d12f
Xuan Choo <xchoo.mainframe@gmail.com>
2012-11-13 17:03:02 -0500

    Fixed bugs introduced when standardizing function parameter names (D, N, etc). Edited demo/basalganglia.py and nps/nps.py to call the make_basalganglia with the appropriate parameter name.

--- simulator-ui/dist-files/demo/basalganglia.py
+++ simulator-ui/dist-files/demo/basalganglia.py
@@ -11,6 +11,6 @@ net.make('output',1,D,mode='direct')
                  #Make a population with 100 neurons, 5 dimensions, and set 
                  #the simulation mode to direct
 nps.basalganglia.make_basal_ganglia(net,'input','output',D,same_neurons=False,
-    N=50)  #Make a basal ganglia model with 50 neurons per action
+    neurons=50)  #Make a basal ganglia model with 50 neurons per action
 net.add_to_nengo()
 
