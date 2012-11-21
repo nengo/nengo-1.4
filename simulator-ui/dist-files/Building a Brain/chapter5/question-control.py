1715620542c40f3693fd2b2bf2894579f77bfb98
tcstewar <tcstewar@uwaterloo.ca>
2012-11-21 15:18:44 -0500

    Changed question-control.py input to use a dictionary rather than a SimpleNode.  This allows the model to be serialized.

--- simulator-ui/dist-files/Building a Brain/chapter5/question-control.py
+++ simulator-ui/dist-files/Building a Brain/chapter5/question-control.py
@@ -56,35 +56,46 @@ gating.make(net,name='Gate2', gated='Motor', neurons=100 ,pstc=0.01)
 net.connect(thalamus, 'Gate1', index_pre=0, func=xBiased)
 net.connect(thalamus, 'Gate2', index_pre=1, func=xBiased)
 
-# Automatic inputs
-class Input(nef.SimpleNode):
-    def __init__(self,name):
-        self.zero=[0]*D
-        nef.SimpleNode.__init__(self,name)
-        self.v1=vocab.parse('STATEMENT+RED*CIRCLE').v
-        self.v2=vocab.parse('STATEMENT+BLUE*SQUARE').v
-        self.v3=vocab.parse('QUESTION+RED').v
-        self.v4=vocab.parse('QUESTION+SQUARE').v
-    def origin_x(self):
-        t=self.t_start
-        if t<1:
-          if 0.1<self.t_start<0.5:
-            return self.v1
-          elif 0.6<self.t_start<1.0:
-            return self.v2
-          else:
-            return self.zero
-        else:
-          t=(t-1.0)%0.6
-          if 0.2<t<0.4:
-            return self.v3
-          elif 0.4<t<0.6:
-            return self.v4            
-          else:
-            return self.zero
-        
-input=Input('Input')
-net.add(input)
-net.connect(input.getOrigin('x'), 'Visual')
+
+inputs={}
+ZERO=[0]*D
+inputs[0.0]=ZERO
+inputs[0.1]=vocab.parse('STATEMENT+RED*CIRCLE').v
+inputs[0.5]=ZERO
+inputs[0.6]=vocab.parse('STATEMENT+BLUE*SQUARE').v
+inputs[1.0]=ZERO
+inputs[1.2]=vocab.parse('QUESTION+RED').v
+inputs[1.4]=vocab.parse('QUESTION+SQUARE').v
+inputs[1.6]=ZERO
+inputs[1.8]=vocab.parse('QUESTION+RED').v
+inputs[2.0]=vocab.parse('QUESTION+SQUARE').v
+inputs[2.2]=ZERO
+inputs[2.4]=vocab.parse('QUESTION+RED').v
+inputs[2.6]=vocab.parse('QUESTION+SQUARE').v
+inputs[2.8]=ZERO
+inputs[3.0]=vocab.parse('QUESTION+RED').v
+inputs[3.2]=vocab.parse('QUESTION+SQUARE').v
+inputs[3.4]=ZERO
+inputs[3.6]=vocab.parse('QUESTION+RED').v
+inputs[3.8]=vocab.parse('QUESTION+SQUARE').v
+inputs[4.0]=ZERO
+inputs[4.2]=vocab.parse('QUESTION+RED').v
+inputs[4.4]=vocab.parse('QUESTION+SQUARE').v
+inputs[4.6]=ZERO
+inputs[4.8]=vocab.parse('QUESTION+RED').v
+inputs[5.0]=vocab.parse('QUESTION+SQUARE').v
+inputs[5.2]=ZERO
+inputs[5.4]=vocab.parse('QUESTION+RED').v
+inputs[5.6]=vocab.parse('QUESTION+SQUARE').v
+inputs[5.8]=ZERO
+inputs[6.0]=vocab.parse('QUESTION+RED').v
+inputs[6.2]=vocab.parse('QUESTION+SQUARE').v
+inputs[6.4]=ZERO
+inputs[6.6]=vocab.parse('QUESTION+RED').v
+inputs[6.8]=vocab.parse('QUESTION+SQUARE').v
+inputs[7.0]=ZERO
+
+net.make_input('Input',inputs)        
+net.connect('Input','Visual')
 
 net.add_to_nengo()
