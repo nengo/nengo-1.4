c096ae770167d682bad0f05e8fc3682658c7ff61
Chris <celiasmi@dyn-129-97-178-252.dynamic.uwaterloo.ca>
2012-11-22 16:58:34 -0500

    Updating tutorials for chp 8

--- simulator-ui/dist-files/Building a Brain/chapter8/2D_decision_integrator.py
+++ simulator-ui/dist-files/Building a Brain/chapter8/2D_decision_integrator.py
@@ -1,15 +1,15 @@
 import nef
 
 net=nef.Network('2D Decision Integrator') 
-input=net.make_input('input',[0,0])  
+input=net.make_input('input', [0, 0])  
 
-net.make('MT',100,2, noise=10) 
-net.make('LIP',200,2, noise=10) 
-net.make('output',100,2,intercept=(.3,1),noise=10)
+net.make('MT', 100, 2, noise=10) 
+net.make('LIP', 200, 2, noise=10) 
+net.make('output', 100, 2, intercept=(0.3, 1), noise=10)
 
-net.connect('input','MT',weight=1,pstc=0.1)  
-net.connect('MT','LIP',weight=.1, pstc=.01) 
-net.connect('LIP','LIP',pstc=0.1)  
-net.connect('LIP','output',pstc=.01)
+net.connect('input', 'MT', pstc=0.01)  
+net.connect('MT', 'LIP', weight=0.1, pstc=0.1) 
+net.connect('LIP', 'LIP', pstc=0.1)  
+net.connect('LIP', 'output', pstc=0.01)
 
 net.add_to_nengo()
