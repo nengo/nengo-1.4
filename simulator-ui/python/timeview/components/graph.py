nengo/nengo-1.4

commit c3a6da99845f00365ad651beba8a29b775368f5a

Changed action handler for dimension checkboxes from "statechanged" to "actionperformed". I noticed that if left at "statechanged", action handler will fire if I mouse over the menu item.

simulator-ui/python/timeview/components/graph.py
@@ -204,7 +204,7 @@ class Graph(core.DataViewComponent):
                         sub_menus[n].add(new_menu)
                         self.popup_dim_menus.append(new_menu)
                         sub_menus[n + 1] = new_menu
-            menu_item = JCheckBoxMenuItem(self.label_for_index(i), draw, stateChanged=lambda x, index=i, self=self: self.select_index(index, x))
+            menu_item = JCheckBoxMenuItem(self.label_for_index(i), draw, actionPerformed=lambda x, index=i, self=self: self.select_index(index, x))
             self.selection_menu_items.append(menu_item)
             sub_menus[num_sub - 1].add(menu_item)
 

commit 6ec8e281315162414f67793bc589f7a4fa3bc1e9

Fixed bugs introduced with commit c3a6da9 (oops!). Now properly updates the graph when you select all and select none.

simulator-ui/python/timeview/components/graph.py
@@ -209,8 +209,9 @@ class Graph(core.DataViewComponent):
             sub_menus[num_sub - 1].add(menu_item)
 
     def select_all(self, event):
-        for item in self.selection_menu_items:
+        for index,item in enumerate(self.selection_menu_items):
             item.setState(True)
+            self.indices[index] = True
         self.sel_all = True
         self.sel_all_menu_item.setState(self.sel_all)
 
@@ -220,8 +221,9 @@ class Graph(core.DataViewComponent):
         self.sel_all_menu_item.setState(self.sel_all)
 
     def select_none(self, event):
-        for item in self.selection_menu_items:
+        for index,item in enumerate(self.selection_menu_items):
             item.setState(False)
+            self.indices[index] = False
         self.sel_all = False
         self.sel_all_menu_item.setState(self.sel_all)
 
@@ -293,6 +295,7 @@ class Graph(core.DataViewComponent):
                 return
 
         self.sel_all = all(self.indices)
+        self.sel_all_menu_item.setState(self.sel_all)
 
         filtered = []
         for i, draw in enumerate(self.indices):