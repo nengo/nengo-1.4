22a36a8a438d9103d98f7ad144d5f1e75d582d26
Chris <celiasmi@dyn-129-97-178-252.dynamic.uwaterloo.ca>
2012-11-29 10:35:19 -0500

    update to book tutorial 1 script

--- simulator-ui/dist-files/Building a Brain/chapter1/singleneuron.py
+++ simulator-ui/dist-files/Building a Brain/chapter1/singleneuron.py
@@ -2,7 +2,7 @@ import nef
 
 net=nef.Network('A Single Neuron (pre-built)')
 net.make_input('input',[0.5])
-net.make('neuron',1,1,max_rate=(100,100),intercept=(-0.5,-0.5),encoders=[[1]],noise=0.1)
+net.make('neuron',1,1,max_rate=(50,100),intercept=(-0.5,0.5),encoders=[[1]],noise=5)
 net.connect('input','neuron')
 net.add_to_nengo()
 
