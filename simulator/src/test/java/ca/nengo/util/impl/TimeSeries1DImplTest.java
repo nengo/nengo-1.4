fa67d16290a0e0e6d55e60bbbf627871b5583805
Jason Sanchez <jason.sanchez@mantech.com>
2012-12-20 10:22:42 -0600

    First unit test created, updated target compiler

--- simulator/src/test/java/ca/nengo/util/impl/TimeSeries1DImplTest.java
+++ simulator/src/test/java/ca/nengo/util/impl/TimeSeries1DImplTest.java
@@ -2,54 +2,43 @@ package ca.nengo.util.impl;
 
 import ca.nengo.model.Units;
 import ca.nengo.util.TimeSeries1D;
-import ca.nengo.util.impl.TimeSeries1DImpl;
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Unit tests for TimeSeries1DImpl. 
  * 
  * @author Bryan Tripp
  */
-public class TimeSeries1DImplTest extends TestCase {
+public class TimeSeries1DImplTest {
 
-	private TimeSeries1D myTimeSeries; 
+	private TimeSeries1D myTimeSeries =
+		new TimeSeries1DImpl(
+			new float[]{0f, 1f},
+			new float[]{1.1f, 2.1f},
+			Units.UNK
+		);
 	
-	protected void setUp() throws Exception {
-		super.setUp();
-		
-		float[] times = new float[]{0f, 1f};
-		float[] values = new float[]{1.1f, 2.1f};		
-		myTimeSeries = new TimeSeries1DImpl(times, values, Units.UNK);
+	@Test(expected=IllegalArgumentException.class)
+	public void constructor() {
+		new TimeSeries1DImpl(new float[]{0f}, new float[]{0f, 1f}, Units.UNK);
 	}
 
-	/*
-	 * Test method for 'ca.bpt.cn.util.impl.TimeSeriesImpl.getTimes()'
-	 */
-	public void testGetTimes() {
+	@Test
+	public void getTimes() {
 		assertEquals(2, myTimeSeries.getTimes().length);
 		assertTrue(myTimeSeries.getTimes()[1] > 0);
 	}
 
-	/*
-	 * Test method for 'ca.bpt.cn.util.impl.TimeSeriesImpl.getValues()'
-	 */
-	public void testGetValues() {
+	@Test
+	public void getValues() {
 		assertEquals(2, myTimeSeries.getValues().length);
 		assertTrue(myTimeSeries.getValues1D()[1] > 2);		
 	}
 
-	/*
-	 * Test method for 'ca.bpt.cn.util.impl.TimeSeriesImpl.getUnits()'
-	 */
-	public void testGetUnits() {
+	@Test
+	public void getUnits() {
 		assertEquals(Units.UNK, myTimeSeries.getUnits1D());
 	}
-	
-	public void testConstructor() {
-		try {
-			new TimeSeries1DImpl(new float[]{0f}, new float[]{0f, 1f}, Units.UNK);
-			fail("Should have thrown exception because of unequal times and values length");
-		} catch (IllegalArgumentException e) {} //exception is expected
-	}
-
 }
