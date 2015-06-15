nengo/nengo-1.4

commit 816a022b35633a25d8d58a8f107e3bdeea5a9923

Remove most uses of import *
We keep having an issue with new versions of Java/Jython in which
import * no longer imports everything we think it does. This should
future-proof us, but not relying on import *.

simulator-ui/python/timeview/components/timecontrol.py
@@ -1,9 +1,11 @@
 import java
 import javax
-from javax.swing import *
-from javax.swing.event import *
-from java.awt import *
-from java.awt.event import *
+from javax.swing import (
+    ImageIcon, JButton, JComboBox, JFileChooser, JFrame, JLabel, JPanel,
+    JSlider, JSpinner, SpinnerNumberModel)
+from javax.swing.event import ChangeListener
+from java.awt import BorderLayout, Color, RenderingHints
+from java.awt.event import ActionListener
 
 from ca.nengo.model import SimulationMode
 
@@ -74,11 +76,11 @@ class TimeControl(JPanel, ChangeListener, ActionListener):
         pdf.maximumSize = pdf.preferredSize
         configPanel.add(pdf)
 
-        data = JPanel(layout=BorderLayout(), opaque=False)
-        data.add(JButton(Icon.data, rolloverIcon=ShadedIcon.data, toolTipText='examine data', actionPerformed=self.show_data, borderPainted=False, focusPainted=False, contentAreaFilled=False))
-        data.add(JLabel('data', horizontalAlignment=javax.swing.SwingConstants.CENTER), BorderLayout.NORTH)
-        data.maximumSize = data.preferredSize
-        configPanel.add(data)
+        self.data = JPanel(layout=BorderLayout(), opaque=False)
+        self.data.add(JButton(Icon.data, rolloverIcon=ShadedIcon.data, toolTipText='examine data', actionPerformed=self.show_data, borderPainted=False, focusPainted=False, contentAreaFilled=False))
+        self.data.add(JLabel('data', horizontalAlignment=javax.swing.SwingConstants.CENTER), BorderLayout.NORTH)
+        self.data.maximumSize = self.data.preferredSize
+        configPanel.add(self.data)
 
         mode = JPanel(layout=BorderLayout(), opaque=False)
         cb = JComboBox(['default', 'rate', 'direct'])
@@ -161,7 +163,7 @@ class TimeControl(JPanel, ChangeListener, ActionListener):
     def show_data(self, event):
         frame = JFrame('%s Data' % self.view.network.name)
         frame.visible = True
-        frame.add(data.DataPanel(self.view))
+        frame.add(self.data.DataPanel(self.view))
         frame.size = (500, 600)
 
     def forward_one_frame(self, event):
@@ -291,14 +293,14 @@ class TimeControl(JPanel, ChangeListener, ActionListener):
 
             cb.addTemplate(tp, 20, 0)
             doc.close()
-            
+
     class RoundedBorder(javax.swing.border.AbstractBorder):
         def __init__(self):
             self.color = Color(0.7, 0.7, 0.7)
-    
+
         def getBorderInsets(self, component):
             return java.awt.Insets(5, 5, 5, 5)
-    
+
         def paintBorder(self, c, g, x, y, width, height):
             g.color = self.color
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)

simulator-ui/python/timeview/data.py
@@ -1,11 +1,11 @@
 import java
-import javax
-from javax.swing import *
+from javax.swing import (
+    JButton, JCheckBoxMenuItem, JFileChooser, JLabel, JPanel, JPopupMenu,
+    JScrollPane, JSpinner, JTable, SpinnerNumberModel)
 from javax.swing.table import DefaultTableModel
-from javax.swing.event import *
-from java.awt import *
-from java.awt.event import *
-from java.io import *
+from java.awt import BorderLayout, Color
+from java.awt.event import MouseEvent, MouseListener
+from java.io import BufferedWriter, File, FileWriter
 from javax.swing.filechooser import FileFilter
 
 import view
@@ -20,18 +20,19 @@ class DataPanel(JPanel,MouseListener):
     def __init__(self,view):
         JPanel.__init__(self)
         self.view=view
-        self.background=Color.white        self.layout=BorderLayout()
+        self.background=Color.white
+        self.layout=BorderLayout()
 
         self.popup=JPopupMenu()
         self.popup_items={}
 
         self.add(self.make_controls(),BorderLayout.SOUTH)
-        
+
         data,title=self.extract_data()
         self.table=JTable(DefaultTableModel(data,title))
-        
+
         scroll=JScrollPane(self.table)
-        self.add(scroll)        
+        self.add(scroll)
 
         scroll.addMouseListener(self)
         self.table.tableHeader.addMouseListener(self)
@@ -40,9 +41,9 @@ class DataPanel(JPanel,MouseListener):
         self.fileChooser=JFileChooser()
         self.fileChooser.setFileFilter(CSVFilter())
         self.fileChooser.setSelectedFile(File('%s.csv'%self.view.network.name))
-        
-        
-        
+
+
+
     def make_controls(self):
         panel=JPanel(background=self.background)
         panel.add(JButton(view.Icon.refresh,actionPerformed=self.refresh,rolloverIcon=view.ShadedIcon.refresh,toolTipText='refresh',borderPainted=False,focusPainted=False,contentAreaFilled=False))
@@ -64,26 +65,26 @@ class DataPanel(JPanel,MouseListener):
         panel.add(JButton(view.Icon.save,actionPerformed=self.save,rolloverIcon=view.ShadedIcon.save,toolTipText='save',borderPainted=False,focusPainted=False,contentAreaFilled=False))
 
         return panel
-        
+
     def extract_data(self):
         pause_state=self.view.paused
         self.view.paused=True
-        
+
         while self.view.simulating:
             java.lang.Thread.sleep(10)
-    
+
         tau=float(self.filter.value)
         dt=self.view.dt
         if tau<dt: dt_tau=None
         else: dt_tau=dt/tau
-        
+
         decimals=int(self.decimals.value)
         format='%%1.%df'%decimals
-    
+
         start_index=max(0,self.view.timelog.tick_count-self.view.timelog.tick_limit+1)
         count=min(self.view.timelog.tick_limit,self.view.timelog.tick_count)
         start_time=start_index*self.view.dt
-        
+
         data=None
         title=['t']
         keys = self.view.watcher.active.keys()
@@ -91,7 +92,7 @@ class DataPanel(JPanel,MouseListener):
         for key in keys:
             watch = self.view.watcher.active[key]
             name,func,args=key
-            
+
             code='%s.%s%s'%(name,func.__name__,args)
             if code not in self.popup_items:
                 state=True
@@ -99,17 +100,17 @@ class DataPanel(JPanel,MouseListener):
                 if 'voltage' in func.__name__: state=False
                 self.popup_items[code]=JCheckBoxMenuItem(code,state,stateChanged=self.refresh)
                 self.popup.add(self.popup_items[code])
-            
+
             if self.popup_items[code].state is False: continue
-                
+
             d=watch.get(dt_tau=dt_tau,start=start_index,count=count)
             n=len(watch.get_first())
             if data is None:
                 data=[]
-                while len(data)<len(d): 
+                while len(data)<len(d):
                     data.append(['%0.4f'%(start_time+(len(data)+0)*self.view.dt)])
-            
-            
+
+
             for i in range(n):
                 title.append('%s[%d]'%(code,i))
                 for j in range(len(data)):
@@ -117,38 +118,37 @@ class DataPanel(JPanel,MouseListener):
                     if dd is None: data[j].append('')
                     else: data[j].append(format%dd[i])
 
-        self.view.paused=pause_state            
+        self.view.paused=pause_state
         return data,title
-        
-        
+
+
     def save(self,event=None):
         if self.fileChooser.showSaveDialog(self)==JFileChooser.APPROVE_OPTION:
             f=self.fileChooser.getSelectedFile()
             writer=BufferedWriter(FileWriter(f))
-            
+
             data,title=self.extract_data()
             title=[t.replace(',',' ') for t in title]
             writer.write(','.join(title)+'\n')
             for row in data:
                 writer.write(','.join(row)+'\n')
-            writer.close()    
-        
+            writer.close()
+
     def refresh(self,event=None):
         data,title=self.extract_data()
         self.table.model.setDataVector(data,title)
-        
+
         
     def mouseClicked(self, event):     
         if event.button==MouseEvent.BUTTON3 or (event.button==MouseEvent.BUTTON1 and event.isControlDown()):
             if self.popup is not None:
-                self.popup.show(event.source,event.x-5,event.y-5)   
+                self.popup.show(event.source,event.x-5,event.y-5)
             
     def mouseEntered(self, event):
         pass
     def mouseExited(self, event):        
         pass
-    def mousePressed(self, event):  
+    def mousePressed(self, event):
         pass
     def mouseReleased(self, event):        
         pass
-    

simulator-ui/python/code_generator.py
@@ -8,17 +8,17 @@ def generate(network):
         if hasattr(node,'getEnsembleFactory'):
             nf=node.getEnsembleFactory().getNodeFactory()
             c="net.make('%s',neurons=%d,dimensions=%d,tau_rc=%g,tau_ref=%g,intercept=(%g,%g),max_rate=(%g,%g))"%(node.name,node.neurons,node.dimension,nf.tauRC,nf.tauRef,nf.intercept.low,nf.intercept.high,nf.maxRate.low,nf.maxRate.high)
-            code.append(c)    
-    
-    importable=['sin','cos','tan','asin','acos','atan','exp','log','pow','sqrt']        
-            
-    imported_funcs=[]        
+            code.append(c)
+
+    importable=['sin','cos','tan','asin','acos','atan','exp','log','pow','sqrt']
+
+    imported_funcs=[]
     for p in network.projections:
         origin=p.origin
-        termination=p.termination  
-        
+        termination=p.termination
+
         connect="'%s','%s'"%(origin.node.name,termination.node.name)
-        
+
         if origin.name!='X':
             funcs=[]
             for f in origin.functions:
@@ -27,7 +27,7 @@ def generate(network):
                     exp=exp.replace('^','**')
                     exp=exp.replace('!',' not ')
                     exp=exp.replace('&',' and ')
-                    exp=exp.replace('|',' or ')                    
+                    exp=exp.replace('|',' or ')
                     exp=exp.replace('ln','log')
                     for f in importable:
                         if f in exp and not f in imported_funcs:
@@ -39,58 +39,50 @@ def generate(network):
                     funcs.append('0')
             code.append('def function(x):')
             code.append('    return [%s]'%(','.join(funcs)))
-            connect=connect+',func=function,origin_name="%s"'%origin.name                
-        
-        
+            connect=connect+',func=function,origin_name="%s"'%origin.name
+
+
         tr='['+','.join(['['+','.join(['%g'%x for x in row])+']' for row in termination.transform])+']'
-        
+
         connect+=',transform=%s'%tr
         # TODO: look for special cases of identify matrixes (and maybe weight, index_pre, and index post?)
-        
+
         code.append('net.connect(%s,pstc=%g)'%(connect,termination.tau))
     if len(imported_funcs)>0:
         code[1:1]=['from math import %s'%','.join(imported_funcs)]
     return '\n'.join(code)
-    
-    
-    
-    
-import java
-import javax
-from javax.swing import *
-from javax.swing.event import *
-from java.awt import *
-from java.awt.event import *
+
+
+from javax.swing import JFrame, JScrollPane, JTextArea, JViewport
+from java.awt import BorderLayout
 import ca.nengo
-    
+
 class CodeView(ca.nengo.util.VisiblyMutable.Listener):
     def __init__(self,network):
         self.network=network
         self.frame=JFrame(network.name)
         self.frame.visible=True
         self.frame.layout=BorderLayout()
-        self.frame.size=(800,600)        
-        
-        
+        self.frame.size=(800,600)
+
+
         self.code=JTextArea(generate(self.network),editable=False)
         scroll=JScrollPane(self.code)
         scroll.viewport.scrollMode=JViewport.SIMPLE_SCROLL_MODE   # not sure why this is needed -- if not done, text is corrupted when scrolling
         self.frame.add(scroll)
-        
+
         self.network.addChangeListener(self)
-        
+
     def changed(self,event):
         self.code.text=generate(self.network)
-        
-        
-        
-"""        
-import nef
-net=nef.Network("GenTest")
-net.make('A',neurons=100,dimensions=2,tau_rc=0.02,tau_ref=0.002,intercept=(-1,1),max_rate=(200,400))
-net.make('B',neurons=100,dimensions=2,tau_rc=0.02,tau_ref=0.002,intercept=(-1,1),max_rate=(30,60))
-net.connect('A','B',transform=[[1,0],[0,1]],pstc=0.01)        
 
-net.add_to_nengo()
-CodeView(net.network)
-"""
+
+if __name__ == '__main__':
+    import nef
+    net=nef.Network("GenTest")
+    net.make('A',neurons=100,dimensions=2,tau_rc=0.02,tau_ref=0.002,intercept=(-1,1),max_rate=(200,400))
+    net.make('B',neurons=100,dimensions=2,tau_rc=0.02,tau_ref=0.002,intercept=(-1,1),max_rate=(30,60))
+    net.connect('A','B',transform=[[1,0],[0,1]],pstc=0.01)
+
+    net.add_to_nengo()
+    CodeView(net.network)

simulator-ui/python/nef/templates/linear_system.py
@@ -2,19 +2,19 @@ title='General Linear System'
 label='Linear\nSystem'
 icon='linearsystem.png'
 
-from ca.nengo.ui.configurable import *
+from ca.nengo.ui.configurable import Property, PropertyInputPanel
 from ca.nengo.ui.configurable.managers import ConfigManager
 from ca.nengo.ui.configurable.descriptors import PCouplingMatrix
 from ca.nengo.util import MU
 from ca.nengo.ui.lib.util import UserMessages
 
-from javax.swing import *
+from javax.swing import JButton, JDialog, JLabel, JTextField
 from javax.swing.event import DocumentListener
 
 class SystemMatrixInputPanel(PropertyInputPanel,DocumentListener):
     def __init__(self,property):
         PropertyInputPanel.__init__(self,property)
-        self.add(JLabel("State dimension: "))    
+        self.add(JLabel("State dimension: "))
         self.state_dim=JTextField(10)
         self.state_dim.document.addDocumentListener(self)
         self.add(self.state_dim)
@@ -44,7 +44,7 @@ class SystemMatrixInputPanel(PropertyInputPanel,DocumentListener):
                 self.matrix=None
         except:
             self.matrix=None
-        
+
     def isValueSet(self):
         return self.matrix is not None
     def getValue(self):
@@ -59,7 +59,7 @@ class SystemMatrixInputPanel(PropertyInputPanel,DocumentListener):
         self.change_dim(event)
     def removeUpdate(self,event):
         self.change_dim(event)
-    
+
 class PSystemMatrix(Property):
     matrix=None
     def createInputPanel(self):
@@ -68,7 +68,7 @@ class PSystemMatrix(Property):
         return "System Matrix"
     def getTypeClass(self):
         return PCouplingMatrix(1,1).getTypeClass()
-        
+
 description="""<html>This is a generic template for constructing a recurrent network that implements the specified dynamic linear system: dx/dt = Ax + Bu. Input Bu must be supplied after construction by adding a termination with the B matrix.</html>"""
 
 params=[
@@ -94,7 +94,7 @@ def make(net,name='System',neurons=100,A=[[0]],tau_feedback=0.1):
     A=numeric.array(A)
     assert len(A.shape)==2
     assert A.shape[0]==A.shape[1]
-    
+
     dimensions=A.shape[0]
     state=net.make(name,neurons,dimensions)
     Ap=A*tau_feedback+numeric.identity(dimensions)
@@ -121,4 +121,3 @@ def make(net,name='System',neurons=100,A=[[0]],tau_feedback=0.1):
         net.network.setMetaData("templateProjections", HashMap())
     templateproj = net.network.getMetaData("templateProjections")
     templateproj.put(name, name)
-    

simulator-ui/python/nef/templates/networkarray.py
@@ -1,6 +1,6 @@
 import nef
-from ca.nengo.ui.configurable import *
-from javax.swing import *
+from ca.nengo.ui.configurable import Property, PropertyInputPanel
+from javax.swing import JComboBox
 from javax.swing.event import DocumentListener
 
 title='Network Array'
@@ -11,8 +11,8 @@ class SignInputPanel(PropertyInputPanel,DocumentListener):
     def __init__(self,property):
         PropertyInputPanel.__init__(self,property)
         self.comboBox = JComboBox(["Unconstrained", "Positive", "Negative"])
-        self.add(self.comboBox)    
-        
+        self.add(self.comboBox)
+
     def isValueSet(self):
         return True
     def getValue(self):
@@ -24,7 +24,7 @@ class SignInputPanel(PropertyInputPanel,DocumentListener):
         else:
             return 0
     def setValue(self, value):
-        pass 
+        pass
 
 class PTemplateSign(Property):
     def createInputPanel(self):
@@ -33,7 +33,7 @@ class PTemplateSign(Property):
         return "Encoder Sign"
     def getTypeClass(self):
         return PInt
-    
+
 description="""<html>This template enables constructing subnetworks full D (# of dimensions) independent populations of neurons.  These are faster to construct but cannot compute all the same nonlinear functions as a single large population with D dimensions.</html>"""
 
 params=[

simulator-ui/python/stats/view/jython/core.py
@@ -1,6 +1,6 @@
-from javax.swing import *
-from java.awt import *
-from java.awt.event import *
+from javax.swing import JFrame
+from java.awt import BorderLayout
+from java.awt.event import ItemEvent
 
 import stats
 
@@ -8,26 +8,26 @@ from .graph import GraphPanel
 from .run import RunPanel
 from .options import OptionsPanel
 from .model import ModelSelectionPanel
-        
 
-                
+
+
 class View:
     def __init__(self):
         self.stats=None
         self.frame=JFrame('Statistics',visible=True,layout=BorderLayout())
-        
+
         self.graph=GraphPanel(self,componentResized=lambda event: self.graph.update())
         self.frame.add(self.graph)
-        
+
         self.options=OptionsPanel(self)
         self.frame.add(self.options,BorderLayout.WEST)
-        
-        
+
+
         self.model_selection=ModelSelectionPanel(self)
         self.frame.add(self.model_selection,BorderLayout.NORTH)
-        
+
         self.frame.add(RunPanel(self),BorderLayout.SOUTH)
-        
+
         self.frame.size=(800,600)
     def selected_params(self):
         return self.options.parameters.get_selected()
@@ -37,16 +37,9 @@ class View:
             self.select_model(event.item)
 
 
-    
+
     def select_model(self,name):
-        self.model_selection.files.selectedItem=name        
+        self.model_selection.files.selectedItem=name
         self.stats=stats.Stats(name)
         self.options.update()
         self.graph.update()
-            
-            
-
-
-        
-        
-        

simulator-ui/python/stats/view/jython/graph.py
@@ -1,6 +1,6 @@
 import java
-from javax.swing import *
-from java.awt import *
+from javax.swing import ImageIcon, JLabel, JPanel
+from java.awt import BorderLayout
 
 import os
 import time
@@ -21,8 +21,8 @@ class GraphPanel(JPanel,java.lang.Runnable):
     def update(self):
         self.should_update=True
         self.graph.enabled=False
-        
-    
+
+
     def run(self):
         while True:
             if self.should_update:
@@ -30,36 +30,37 @@ class GraphPanel(JPanel,java.lang.Runnable):
                 self.do_update()
             if self.should_update:
                 self.graph.enabled=False
-            else:    
-                time.sleep(0.1)    
-    def do_update(self):            
+            else:
+                time.sleep(0.1)
+    def do_update(self):
         if self.view.stats is None: return
         if len(self.view.stats.data)==0: return
         #print 'update graph'
-        
+
         for f in os.listdir('python'):
             if f.startswith('.view.') and f.endswith('.png'):
                 os.remove('python/'+f)
-        
+
         fn='python/.view.%08x'%random.randrange(0x7fffffff)
-        
+
         code=self.make_pylab_code(fn+'.png')
         #print code
         f=open(fn+'.py','w')
         f.write(code)
         f.close()
         os.system('python %s.py'%fn)
-        
+
         icon=ImageIcon(fn+'.png')
         self.graph.icon=icon
         self.graph.enabled=True
         os.remove(fn+'.py')
         self.revalidate()
-    
+
     def make_pylab_code(self,filename):
         opt=self.view.options
+        g_opt = getattr(opt, 'global')
 
-        dpi=opt.global.dpi.text
+        dpi=g_opt.dpi.text
         try: dpi=int(dpi)
         except: dpi=80
 
@@ -71,30 +72,29 @@ class GraphPanel(JPanel,java.lang.Runnable):
         for k,v in params.items():
             if isinstance(v,list) and len(v)==1: params[k]=v[0]
         lines.append('s=s(%s)'%','.join(['%s=%s'%(k,`v`) for k,v in params.items()]))
-        
+
         lines.append('import stats.plot')
-        color=opt.global.opt_color.isSelected()
+        color=g_opt.opt_color.isSelected()
         values=self.view.stats.data[0].computed.value_names()
-        metric=opt.global.metrics.selectedItem
-        
+        metric=g_opt.metrics.selectedItem
+
         width=self.size.width/float(dpi)
         height=self.size.height/float(dpi)
         if opt.get_graph_type()=='bar':
             lines.append('p=stats.plot.Bar(width=%g,height=%g,color=%s)'%(
-                                    width,height,color))        
+                                    width,height,color))
             scatter=opt.bar_tab.show_samples.isSelected()
             lines.append('p.plot(s,values=%s,scatter=%s,metric=%s)'%(`values`,`scatter`,`metric`))
         elif opt.get_graph_type()=='line':
-            
-            spaced=opt.line_tab.x_axis.selectedItem=='equal'        
-            logx=opt.line_tab.x_axis.selectedItem=='log'        
-            logy=opt.line_tab.y_axis.selectedItem=='log'        
+
+            spaced=opt.line_tab.x_axis.selectedItem=='equal'
+            logx=opt.line_tab.x_axis.selectedItem=='log'
+            logy=opt.line_tab.y_axis.selectedItem=='log'
             lines.append('p=stats.plot.Line(width=%g,height=%g,color=%s,log_x=%s,log_y=%s,spaced=%s)'%(
-                                    width,height,color,logx,logy,spaced)) 
+                                    width,height,color,logx,logy,spaced))
             area=opt.line_tab.area.isSelected()
             lines.append('p.plot(s,values=%s,area=%s,metric=%s)'%(`values`,`area`,`metric`))
-        
-        lines.append('p.save("%s",dpi=%d)'%(filename,dpi)) 
-        
-        return '\n'.join(lines)
 
+        lines.append('p.save("%s",dpi=%d)'%(filename,dpi))
+
+        return '\n'.join(lines)

commit 30bc7af309265171ffff823d9c67387dfae78372

Fix data output from failing
Commit 816a022b35633a25d8d58a8f107e3bdeea5a9923 broke the "data"
output plots from the interactive mode display (the thing for
looking at and exporting the raw data from the model).

simulator-ui/python/timeview/components/timecontrol.py
@@ -8,7 +8,7 @@ from java.awt import BorderLayout, Color, RenderingHints
 from java.awt.event import ActionListener
 
 from ca.nengo.model import SimulationMode
-
+import timeview.data
 import os
 
 # for save_pdf
@@ -163,7 +163,7 @@ class TimeControl(JPanel, ChangeListener, ActionListener):
     def show_data(self, event):
         frame = JFrame('%s Data' % self.view.network.name)
         frame.visible = True
-        frame.add(self.data.DataPanel(self.view))
+        frame.add(timeview.data.DataPanel(self.view))
         frame.size = (500, 600)
 
     def forward_one_frame(self, event):

simulator-ui/python/timeview/data.py
@@ -8,7 +8,6 @@ from java.awt.event import MouseEvent, MouseListener
 from java.io import BufferedWriter, File, FileWriter
 from javax.swing.filechooser import FileFilter
 
-import view
 
 class CSVFilter(FileFilter):
     def accept(self,f):
@@ -45,8 +44,9 @@ class DataPanel(JPanel,MouseListener):
 
 
     def make_controls(self):
+        from timeview.components.timecontrol import Icon, ShadedIcon
         panel=JPanel(background=self.background)
-        panel.add(JButton(view.Icon.refresh,actionPerformed=self.refresh,rolloverIcon=view.ShadedIcon.refresh,toolTipText='refresh',borderPainted=False,focusPainted=False,contentAreaFilled=False))
+        panel.add(JButton(Icon.refresh,actionPerformed=self.refresh,rolloverIcon=ShadedIcon.refresh,toolTipText='refresh',borderPainted=False,focusPainted=False,contentAreaFilled=False))
 
         filter=JPanel(layout=BorderLayout(),opaque=False)
         self.filter = JSpinner(SpinnerNumberModel(self.view.tau_filter,0,0.5,0.01),stateChanged=self.refresh)
@@ -62,7 +62,7 @@ class DataPanel(JPanel,MouseListener):
         decimals.maximumSize=decimals.preferredSize
         panel.add(decimals)
 
-        panel.add(JButton(view.Icon.save,actionPerformed=self.save,rolloverIcon=view.ShadedIcon.save,toolTipText='save',borderPainted=False,focusPainted=False,contentAreaFilled=False))
+        panel.add(JButton(Icon.save,actionPerformed=self.save,rolloverIcon=ShadedIcon.save,toolTipText='save',borderPainted=False,focusPainted=False,contentAreaFilled=False))
 
         return panel
 